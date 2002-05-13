348373ebbfbb71d756045af538735a349f907942
Christian Tanzer <tanzer@swing.co.at>
2002-05-13 09:50:40 +0000

    Recent changes

---
+++ _TFL/_Meta/__init__.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python
+# Copyright (C) 2001 Mag. Christian Tanzer. All rights reserved
+# Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
+# ****************************************************************************
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the Free
+# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# ****************************************************************************
+#
+#++
+# Name
+#    TFL/Meta/__init__
+#
+# Purpose
+#    Initialize package `TFL.Meta`
+#
+# Revision Dates
+#    13-May-2002 (CT) Creation
+#    ««revision-date»»···
+#--
+
+from Package_Namespace import Package_Namespace
+
+Meta = Package_Namespace ()
+
+del Package_Namespace
+
+### __END__ TFL/__init__
