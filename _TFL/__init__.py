d6f1d3eb774e43a580a2717ede7a0b14f2e85705
Christian Tanzer <tanzer@swing.co.at>
2002-06-24 11:21:29 +0000

    Recent changes

--- _TFL/__init__.py
+++ _TFL/__init__.py
@@ -2,17 +2,17 @@
 # Copyright (C) 2001 Mag. Christian Tanzer. All rights reserved
 # Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
 # ****************************************************************************
-# 
+#
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Library General Public
 # License as published by the Free Software Foundation; either
 # version 2 of the License, or (at your option) any later version.
-# 
+#
 # This library is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 # Library General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU Library General Public
 # License along with this library; if not, write to the Free
 # Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -29,16 +29,17 @@
 #     3-Jul-2001 (CT) Creation (of comment)
 #    22-Feb-2002 (CT) `_Export` for `Package_Namespace` added
 #    27-Feb-2002 (CT) `TFL.Package_Namespace` assigned instead of using
-#                     `_Export` (which leads to circular import again) 
+#                     `_Export` (which leads to circular import again)
+#    24-Jun-2002 (CT) Import `Package_Namespace` absolutely (i.e., from `_TFL`)
 #    ««revision-date»»···
 #--
 
-from Package_Namespace import Package_Namespace
+from _TFL.Package_Namespace import Package_Namespace
 
 TFL = Package_Namespace ()
 
 ### We Package_Namespace to `TFL` to avoid circular imports between TFL and
-### Package_Namespace 
+### Package_Namespace
 TFL.Package_Namespace = Package_Namespace
 
 del Package_Namespace
