93ad6269d7e5dbf9fd73233ae5d78768b00a689a
Hermann Maier <office@tttech.com>
2004-05-11 13:40:23 +0000

    Added tex_quoted_no_underscore and tex_quoted_underscore_word_with_path

--- _TFL/tex_quoted.py
+++ _TFL/tex_quoted.py
@@ -26,16 +26,26 @@
 #    Quote TeX-critical characters
 #
 # Revision Dates
-#    29-Jan-2004 (CT) Creation
-#    ««revision-date»»···
+#    29-Jan-2004 (CT)  Creation
+#    11-May-2004 (RMA) Added tex_quoted_no_underscore and
+#                      tex_quoted_underscore_word_with_path
+#    ««revision-date»»
 #--
 
 from Regexp import *
 
 _tex_pi_symbols = Regexp (r"[«»]", re.X)
-_tex_to_quote   = Regexp (r"([_^\\#~%&${}])", re.X)
+_tex_to_quote   = Regexp (r"([\\#~%&${}^])", re.X)
+_tex_underscore = Regexp (r"([_])", re.X)
 _tex_tt_symbols = Regexp (r"[<>+*|]", re.X)
 _tex_diacritics = Regexp (r"[äöüÄÖÜß«»]")
+
+_word_boundaries= "[^ (),\t\n\r\f\v]"
+
+_tex_path_text  = Regexp (r"(%s*?[_]%s*)" \
+                         % (_word_boundaries, _word_boundaries)
+                         , re.X
+                         )
 
 def _tex_subs_pi_symbols (match) :
     m = match.group (0)
@@ -60,9 +70,22 @@ def _tex_subs_diacritics (match) :
            }.get (m, m)
 # end def _tex_subs_diacritics
 
+def _tex_subs_to_path (match) :
+    return r"\small{\path|%s|}" % (match.group (0), )
+# end def _tex_subs_to_path
+
 def tex_quoted (s) :
     """Return `s` with all TeX-critical characters quoted in some adequate,
        if ugly, way.
+    """
+    s = tex_quoted_no_underscore (s)
+    if isinstance (s, (str, unicode)) :
+        s = _tex_underscore.sub   (_tex_subs_to_quote,   s)
+    return s
+# end def tex_quoted
+
+def tex_quoted_no_underscore (s) :
+    """Same as tex_quoted but does NOT quote underscores.
     """
     if isinstance (s, (str, unicode)) :
         s = _tex_pi_symbols.sub (_tex_subs_pi_symbols, s)
@@ -70,7 +93,15 @@ def tex_quoted (s) :
         s = _tex_tt_symbols.sub (_tex_subs_tt_symbols, s)
         s = _tex_diacritics.sub (_tex_subs_diacritics, s)
     return s
-# end def tex_quoted
+# end def tex_quoted_no_underscore
+
+def tex_quoted_underscore_word_with_path (s) :
+    if isinstance (s, (str, unicode)) :
+        s = _tex_path_text.sub (_tex_subs_to_path, s)
+    return s
+# end def tex_quoted_underscore_word_with_path
+
+
 
 ### unit-test code ############################################################
 
