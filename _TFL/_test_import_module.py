4b80ad5a92e2763898535be76103b5b98d5c601c
Christian Tanzer <tanzer@swing.co.at>
2004-06-17 09:00:28 +0000

    Recent changes

---
+++ _TFL/_test_import_module.py
@@ -0,0 +1,69 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2004 Mag. Christian Tanzer. All rights reserved
+# Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.cluster
+# ****************************************************************************
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the Free
+# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# ****************************************************************************
+#
+#++
+# Name
+#    _test_import_module
+#
+# Purpose
+#    Test if `import_module` works correctly
+#
+# Revision Dates
+#    17-Jun-2004 (CT) Creation
+#    ««revision-date»»···
+#--
+
+"""This module tests _TFL.import_module in two ways. The doctest tests the
+normal use of the module and can be triggered by running this module through
+/swing/python/run_doctest.py as in
+
+    python /swing/system/python/run_doctest.py \
+        lib/python/_TFL/_test_import_module.py -p lib/python/_TFL
+
+The other way tests the use of `import_module` for importing a module while
+it is still being imported. This can be tested by importing this module into
+a running python interpreter as in
+
+    import _TFL._test_import_module
+
+>>> import_module ("_TFL.import_module").__name__
+'_TFL.import_module'
+>>> import_module ("os").__name__
+'os'
+"""
+
+from _TFL.import_module import *
+
+import U_Test
+
+def _doc_test () :
+    return U_Test.run_module_doc_tests ("_TFL._test_import_module")
+# end def _doc_test
+
+_doc_test ()
+
+print "Importing ", import_module ("_TFL._test_import_module"), "works"
+try :
+    import_module_brittle ("_TFL._test_import_module")
+except ImportError, exc :
+    print ("Importing _TFL._test_import_module with "
+           "import_module_brittle fails as expected: \n    "
+          ), exc
+### __END__ _test_import_module
--- _TFL/import_module.py
+++ _TFL/import_module.py
@@ -27,10 +27,16 @@
 #
 # Revision Dates
 #     9-Dec-2003 (CT) Creation
+#    17-Jun-2004 (CT) `import_module` changed to use `sys.modules` to get the
+#                     result instead of a `getattr` loop
+#                     [the old implementation renamed to
+#                     `import_module_brittle`]
 #    ««revision-date»»···
 #--
 
-def import_module (module_name) :
+import sys
+
+def import_module_brittle (module_name) :
     """Returns the imported module named `module_name`. If `module_name` is
        naming a module living inside a package (e.g., `A.B.C`), the rightmost
        module (i.e., `C`) is returned.
@@ -47,17 +53,29 @@ def import_module (module_name) :
     ### When importing a module from a package, note that
     ### __import__('A.B', ...) returns package A when fromlist is empty,
     ### but its submodule B when fromlist is not empty.
-    ###
-    result = __import__ (module_name, {}, {}, ())
+    result = __import__ (module_name)
     for n in module_name.split (".") [1:] :
+        ### This kind of importing is brittle because it fails if one tries to
+        ### `import_module_brittle` a module inside a package while the module
+        ### itself is still being imported (it is already in sys.modules but
+        ### not yet in the `__dict__` of the containing package)
         try :
             result = getattr (result, n)
         except AttributeError, exc :
             raise ImportError, "%s: %s, %s" % (exc, module_name, result)
     return result
+# end def import_module_brittle
+
+def import_module (module_name) :
+    """Returns the imported module named `module_name`. If `module_name` is
+       naming a module living inside a package (e.g., `A.B.C`), the rightmost
+       module (i.e., `C`) is returned.
+    """
+    __import__ (module_name)
+    return sys.modules [module_name]
 # end def import_module
 
 if __name__ != "__main__" :
     from _TFL import TFL
-    TFL._Export ("*")
+    TFL._Export ("import_module")
 ### __END__ import_module
