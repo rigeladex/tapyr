14fd526edb8a7569930f5e647258413ba271282d
Christian Tanzer <tanzer@swing.co.at>
2005-02-20 13:07:52 +0000

    Recent changes

---
+++ _TFL/Apply_All.py
@@ -0,0 +1,80 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2005 Mag. Christian Tanzer. All rights reserved
+# Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.cluster
+# ****************************************************************************
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the Free
+# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# ****************************************************************************
+#
+#++
+# Name
+#    Apply_All
+#
+# Purpose
+#    Class transparently applying method calls to a set of objects
+#
+# Revision Dates
+#    20-Feb-2005 (CT) Creation
+#    ««revision-date»»···
+#--
+
+from   _TFL              import TFL
+import _TFL._Meta.Object
+
+class Apply_All (TFL.Meta.Object) :
+    """Class transparently applying method calls to a set of objects.
+
+       >>> l1  = range (5)
+       >>> l2  = ["f", "b", "c", "a"]
+       >>> all = Apply_All (l1, l2)
+       >>> all._reveivers
+       ([0, 1, 2, 3, 4], ['f', 'b', 'c', 'a'])
+       >>> all.sort ()
+       >>> all._reveivers
+       ([0, 1, 2, 3, 4], ['a', 'b', 'c', 'f'])
+       >>> all.count ("a")
+       [0, 1]
+       >>> all.reverse ()
+       >>> all._reveivers
+       ([4, 3, 2, 1, 0], ['f', 'c', 'b', 'a'])
+       >>> all.pop ()
+       [0, 'a']
+       >>> all._reveivers
+       ([4, 3, 2, 1], ['f', 'c', 'b'])
+    """
+
+    def __init__ (self, * receivers) :
+        self._reveivers = receivers
+    # end def __init__
+
+    def _apply (self, name, * args, ** kw) :
+        result = []
+        for r in self._reveivers :
+            f = getattr (r, name)
+            r = f (* args, ** kw)
+            if r is not None :
+                result.append (r)
+        return result or None
+    # end def _apply
+
+    def __getattr__ (self, name) :
+        return lambda * args, ** kw : self._apply (name, * args, ** kw)
+    # end def __getattr__
+
+# end class Apply_All
+
+if __name__ != "__main__" :
+    TFL._Export ("*")
+### __END__ Apply_All
---
+++ _TFL/_TKT/_Tk/Widget.py
@@ -0,0 +1,77 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2005 Mag. Christian Tanzer. All rights reserved
+# Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
+# ****************************************************************************
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the Free
+# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# ****************************************************************************
+#
+#++
+# Name
+#    TFL.TKT.Tk.Widget
+#
+# Purpose
+#    Model widget for Tkinter based GUI
+#
+# Revision Dates
+#    20-Feb-2005 (CT) Creation
+#    ««revision-date»»···
+#--
+
+from   _TFL                 import TFL
+import _TFL._TKT.Mixin
+import _TFL._TKT._Tk
+
+
+class Widget (TFL.TKT.Mixin) :
+    """Model widget for Tkinter based GUI"""
+
+    def apply_style (self, style, * args, ** kw) :
+        w = self.wtk_widget
+        w.configure (** self._styler (style).option_dict)
+        self._apply_style_bindings   (style)
+        if style.mouse_cursor is not None :
+            ### `cursor` can be `configure`d with option `cursor`
+            ### XXX but should we support the `restore` cursor functionality
+            ### XXX supported by the CT_TK methods
+            ### XXX `busy_cursor`/`normal_cursor` ???
+            pass ### XXX change cursor
+    # end def apply_style
+
+    def remove_style (self, style) :
+        ### XXX ??? does this make sense ???
+        pass
+    # end def apply_bitmap
+
+    def _apply_style_bindings (self, style, binder = None) :
+        if style is not None and style.callback :
+            if binder is None :
+                binder = self.wtk_widget.bind
+            for name, cb in style.callback.iteritems () :
+                binder (getattr (self.TNS.Eventname, name), cb)
+    # end def _apply_style_bindings
+
+    def _styler (self, style) :
+        sty_map = self._sty_map
+        if style not in sty_map :
+            sty_map [style] = self.Styler (style)
+        return sty_map [style]
+    # end def _styler
+
+# end class Widget
+
+if __name__ != "__main__" :
+    TFL.TKT.Tk._Export ("*")
+### __END__ TFL.TKT.Tk.Widget
