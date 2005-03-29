0bd4f4a4fe82db0761a53b90d46076c5208fd320
Christian Tanzer <tanzer@swing.co.at>
2005-03-29 15:37:26 +0000

    Recent changes

---
+++ _PMA/_TKT/__init__.py
@@ -0,0 +1,43 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2004 Mag. Christian Tanzer. All rights reserved
+# Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
+# ****************************************************************************
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the Free
+# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# ****************************************************************************
+#
+#++
+# Name
+#    PMA.TKT.__init__
+#
+# Purpose
+#    Package providing (UI)-toolkit support for PMA
+#
+# Revision Dates
+#    29-Mar-2005 (CT) Creation
+#    ««revision-date»»···
+#--
+
+from   _TFL.Package_Namespace import Derived_Package_Namespace
+from   _PMA                   import PMA
+from   _TGL                   import TGL
+import _TGL._TKT
+
+TKT = Derived_Package_Namespace (parent = TGL.TKT)
+PMA._Export ("TKT")
+
+del Derived_Package_Namespace
+
+### __END__ PMA.TKT.__init__
