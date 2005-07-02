59238d09d93cb99637c0fdc3cb180fdb7f4a4891
Christian Tanzer <tanzer@swing.co.at>
2005-07-02 18:39:13 +0000

    Recent changes

--- _TFL/DL_List.py
+++ _TFL/DL_List.py
@@ -35,6 +35,12 @@
 #     2-Dec-2003 (CT)  Already commented-out `__getattr__` finally removed
 #     9-Mar-2004 (CT)  `_doc_test` changed to not use `import`
 #    30-Jun-2005 (CT)  Style improvements
+#     2-Jul-2005 (CT)  `_DL_Chain_` factored
+#     2-Jul-2005 (CT)  Class attribute `DL_Item` added to `_DL_Chain_`
+#     2-Jul-2005 (CT)  `DL_Item.predecessors` and `DL_Item.successors`
+#                      changed to break at `self` (to allow cyclic structures)
+#     2-Jul-2005 (CT)  `DL_Ring` added
+#     2-Jul-2005 (CT)  `_DL_Counted_` factored and `DL_Ring_Counted` added
 #    ««revision-date»»···
 #--
 
@@ -64,24 +70,28 @@ class DL_Item (TFL.Meta.Object) :
     # end def link_prev
 
     def predecessors (self) :
-        """Iterator over predecessors of `self`."""
+        """Generate predecessors of `self`."""
         p = self
         while p.prev :
             p = p.prev
+            if p is self :
+                break
             yield p
     # end def predecessors
 
     def successors (self) :
-        """Iterator over successors of `self`."""
+        """Generate successors of `self`."""
         p = self
         while p.next :
             p = p.next
+            if p is self :
+                break
             yield p
     # end def successors
 
     def resplice (self, h, t) :
-        """Move DL_Items from `h` to `t` after `self` (removing that sequence
-           wherever it lived before).
+        """Move DL_Items from `h` to `t` after `self`
+           (removing that sequence wherever it lived before).
         """
         assert bool (h) and bool (t), "resplice %s: %s %s" % (self, h, t)
         h.prev.link_next (t.next)
@@ -109,50 +119,34 @@ class DL_Item (TFL.Meta.Object) :
 
 # end class DL_Item
 
-class DL_List (TFL.Meta.Object) :
-    """Doubly linked list.
+class _DL_Chain_ (TFL.Meta.Object) :
+    """Root class for doubly linked chains."""
 
-       >>> dl = DL_List (0, 1, 2, 3, 4)
-       >>> list (dl)
-       [0, 1, 2, 3, 4]
-       >>> for x in dl :
-       ...   print x
-       ...
-       0
-       1
-       2
-       3
-       4
-       >>> dl.head
-       0
-       >>> dl.tail
-       4
-    """
+    DL_Item = DL_Item
 
     head = property (lambda s : s._H.next)
     tail = property (lambda s : s._T.prev)
 
     def __init__ (self, * items) :
-        self._H = DL_Item ()
-        self._T = DL_Item ()
         self.clear  ()
         self.append (* items)
     # end def __init__
 
     def append (self, * items) :
+        """Append `items` at end of list"""
         self.insert (self._T.prev, * items)
     # end def append
 
-    def clear (self) :
-        self._H.link_next (self._T)
-    # end def clear
-
     def insert (self, pred, * items) :
+        """Insert `items` after `pred`"""
         for item in items :
             pred = self._new_item (pred, item)
     # end def insert
 
     def item (self, index) :
+        """Return item at `index` (starting from 0).
+           Beware of O(n) behavior for a single call.
+        """
         if index >= 0 :
             i = index
             r = self.head
@@ -170,68 +164,58 @@ class DL_List (TFL.Meta.Object) :
         return r
     # end def item
 
-    def __iter__ (self) :
-        return self._H.successors ()
-    # end def __iter__
-
-    def _new_item (self, pred, item) :
-        return DL_Item (item, pred.next, pred)
-    # end def _new_item
-
     def pop (self) :
+        """Remove and return last element."""
         return self.remove (self.tail)
     # end def pop
 
     def pop_front (self) :
+        """Remove and return first element."""
         return self.remove (self.head)
     # end def pop_front
 
     def prepend (self, * items) :
+        """Prepend `items` at front of list."""
         self.insert (self._H, * items)
     # end def prepend
 
     def remove (self, item) :
-        if item is self._H or item is self._T :
-            raise IndexError, error
+        """Remove `item` from its list."""
         item.prev.link_next (item.next)
         return item.value
     # end def remove
 
     def reverse_iter (self) :
+        """Generate items in list in reverse order."""
         return self._T.predecessors ()
     # end def reverse_iter
 
     def reverse_values (self) :
+        """Generate values in list in reverse order."""
         for item in self.reverse_iter () :
             yield item.value
     # end def reverse_values
 
     def values (self) :
+        """Generate values in list."""
         for item in iter (self) :
             yield item.value
     # end def values
 
     itervalues = values ### compatibility to `dict`
 
-    def __nonzero__ (self) :
-        return self._H.next is not self._T
-    # end def __nonzero__
+    def _new_item (self, pred, item) :
+        return self.DL_Item (item, pred.next, pred)
+    # end def _new_item
 
-# end class DL_List
+    def __iter__ (self) :
+        return self._H.successors ()
+    # end def __iter__
 
-class DL_List_Counted (DL_List) :
-    """DL_List counting its elements
+# end class _DL_Chain_
 
-       >>> dlc = DL_List_Counted (* range (5))
-       >>> dlc.count
-       5
-       >>> dlc.pop()
-       >>> len (dlc)
-       4
-       >>> dlc.append (42)
-       >>> len (dlc)
-       5
-    """
+class _DL_Counted_ (TFL.Meta.Object) :
+    """Mixin counting elements."""
 
     def clear (self) :
         self.__super.clear ()
@@ -252,7 +236,231 @@ class DL_List_Counted (DL_List) :
         return self.count
     # end def __len__
 
+# end class _DL_Counted_
+
+class DL_List (_DL_Chain_) :
+    """Doubly linked list.
+
+       >>> dl = DL_List (0, 1, 2, 3, 4)
+       >>> list (dl)
+       [0, 1, 2, 3, 4]
+       >>> dl.head, dl.tail
+       (0, 4)
+       >>> list (dl.head.successors ())
+       [1, 2, 3, 4]
+       >>> list (dl.tail.predecessors ())
+       [3, 2, 1, 0]
+       >>> dl.item (2)
+       2
+       >>> dl.item (5)
+       Traceback (most recent call last):
+       ...
+       IndexError: 5
+       >>> dl.prepend (42)
+       >>> list (dl), list (dl.reverse_values ())
+       ([42, 0, 1, 2, 3, 4], [4, 3, 2, 1, 0, 42])
+       >>> dl.pop_front ()
+       42
+       >>> dl.pop ()
+       4
+       >>> dl.remove (dl.head.next)
+       1
+       >>> for x in dl :
+       ...   print x
+       ...
+       0
+       2
+       3
+       >>> dl.clear ()
+       >>> list (dl)
+       []
+       >>> dl = DL_List (0, 1, 2, 3, 4)
+       >>> dk = DL_List ("a", "b", "c", "d", "e")
+       >>> dl.head.resplice (dl.tail.prev, dl.tail)
+       >>> list (dl), list (dk)
+       ([0, 3, 4, 1, 2], ['a', 'b', 'c', 'd', 'e'])
+       >>> dk.head.next.next.resplice (dl.head.next, dl.tail.prev)
+       >>> list (dl), list (dk)
+       ([0, 2], ['a', 'b', 'c', 3, 4, 1, 'd', 'e'])
+    """
+
+    head    = property (lambda s : s._H.next)
+    tail    = property (lambda s : s._T.prev)
+
+    def __init__ (self, * items) :
+        self._H = self.DL_Item ()
+        self._T = self.DL_Item ()
+        self.__super.__init__  (* items)
+    # end def __init__
+
+    def clear (self) :
+        """Remove all items from list."""
+        self._H.link_next (self._T)
+    # end def clear
+
+    def remove (self, item) :
+        if item is self._H or item is self._T :
+            raise IndexError, error
+        return self.__super.remove (item)
+    # end def remove
+
+    def __nonzero__ (self) :
+        return self._H.next is not self._T
+    # end def __nonzero__
+
+# end class DL_List
+
+class DL_List_Counted (_DL_Counted_, DL_List) :
+    """DL_List counting its elements
+
+       >>> dlc = DL_List_Counted (* range (5))
+       >>> dlc.count
+       5
+       >>> dlc.pop()
+       >>> len (dlc)
+       4
+       >>> dlc.append (42)
+       >>> len (dlc)
+       5
+    """
+
 # end class DL_List_Counted
+
+class DL_Ring (_DL_Chain_) :
+    """Doubly linked ring.
+
+       >>> dr = DL_Ring (0, 1, 2, 3, 4)
+       >>> list (dr)
+       [0, 1, 2, 3, 4]
+       >>> dr.head, dr.tail
+       (0, 4)
+       >>> list (dr.head.successors ()), list (dr.head.predecessors ())
+       ([1, 2, 3, 4], [4, 3, 2, 1])
+       >>> list (dr.tail.successors ()), list (dr.tail.predecessors ())
+       ([0, 1, 2, 3], [3, 2, 1, 0])
+       >>> dr.item (2)
+       2
+       >>> dr.item (5)
+       0
+       >>> dr.prepend (42)
+       >>> list (dr), list (dr.reverse_values ())
+       ([42, 0, 1, 2, 3, 4], [4, 3, 2, 1, 0, 42])
+       >>> dr.pop_front ()
+       42
+       >>> dr.pop ()
+       4
+       >>> dr.remove (dr.head.next)
+       1
+       >>> for x in dr :
+       ...   print x
+       ...
+       0
+       2
+       3
+       >>> dr.clear ()
+       >>> list (dr)
+       []
+       >>> print dr.head, dr.tail
+       None None
+       >>> dr = DL_Ring (0, 1, 2, 3, 4)
+       >>> list (dr)
+       [0, 1, 2, 3, 4]
+       >>> dr.rotate_next (4)
+       >>> list (dr)
+       [4, 0, 1, 2, 3]
+       >>> dr.rotate_prev (1)
+       >>> list (dr)
+       [3, 4, 0, 1, 2]
+       >>> dr.rotate_prev (3)
+       >>> list (dr)
+       [0, 1, 2, 3, 4]
+    """
+
+    head    = property (lambda s : s._H and s._H.next)
+    tail    = property (lambda s : s._T.prev)
+    _H      = property (lambda s : s.mark.prev)
+    _T      = property (lambda s : s.mark)
+    _NIL    = DL_Item  ()
+
+    def clear (self) :
+        """Remove all items from list."""
+        self.mark = self._NIL
+    # end def clear
+
+    def insert (self, pred, * items) :
+        if items :
+            if self.mark is self._NIL :
+                pred  = self.mark = self.DL_Item (items [0])
+                items = items   [1:]
+                pred.link_next  (pred)
+            self.__super.insert (pred, * items)
+    # end def insert
+
+    def prepend (self, * items) :
+        self.__super.prepend (* items)
+        self.rotate_prev     (len (items))
+    # end def prepend
+
+    def remove (self, item) :
+        if item is self._NIL :
+            raise IndexError, error
+        elif item.next is item.prev is item :
+            assert item is self.mark
+            self.clear ()
+            return item
+        else :
+            if item is self.mark :
+                self.mark = item.next
+            return self.__super.remove (item)
+    # end def remove
+
+    def reverse_iter (self) :
+        if self.mark is not self._NIL :
+            for p in self._T.predecessors () :
+                yield p
+            yield self._T
+    # end def reverse_iter
+
+    def rotate_next (self, n) :
+        """Move `self.mark` forward (i.e., following `next`) `n` times."""
+        for i in xrange (n) :
+            self.mark = self.mark.next
+    # end def rotate_next
+
+    def rotate_prev (self, n) :
+        """Move `self.mark` backward (i.e., following `prev`) `n` times."""
+        for i in xrange (n) :
+            self.mark = self.mark.prev
+    # end def rotate_prev
+
+    def __iter__ (self) :
+        if self.mark is not self._NIL :
+            for s in self._H.successors () :
+                yield s
+            yield self._H
+    # end def __iter__
+
+    def __nonzero__ (self) :
+        return self.mark is not self._NIL
+    # end def __nonzero__
+
+# end class DL_Ring
+
+class DL_Ring_Counted (_DL_Counted_, DL_Ring) :
+    """DL_Ring counting its elements
+
+       >>> drc = DL_Ring_Counted (* range (5))
+       >>> drc.count
+       5
+       >>> drc.pop()
+       >>> len (drc)
+       4
+       >>> drc.append (42)
+       >>> len (drc)
+       5
+    """
+
+# end class DL_Ring_Counted
 
 if __name__ != "__main__" :
     TFL._Export ("*")
