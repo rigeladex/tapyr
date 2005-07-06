c4997e4c3fb07ead5eb6cfe0ffa7cfebe7326eda
Christian Tanzer <tanzer@swing.co.at>
2005-07-06 09:58:54 +0000

    Recent changes

--- _TFL/normalized_indent.py
+++ _TFL/normalized_indent.py
@@ -27,66 +27,80 @@
 #
 # Revision Dates
 #    14-Mar-2005 (CT) Creation
+#     6-Jul-2005 (CT) Doctest moved from function `normalized_indent` to
+#                     module (and another case added)
+#     6-Jul-2005 (CT) `textwrap.dedent` used if available
 #    ««revision-date»»···
 #--
 
+"""
+>>> normalized_indent ("Just a single line.")
+'Just a single line.'
+>>> normalized_indent ("  Just a single line inside white space.  ")
+'Just a single line inside white space.'
+>>> print _normalized_indent ('''First line.
+...     Second line.
+...     Third line.
+...         Fourth line (indented).
+...         Fifth line (ditto).
+...     Sixth line.
+... ''')
+First line.
+Second line.
+Third line.
+    Fourth line (indented).
+    Fifth line (ditto).
+Sixth line.
+>>> print normalized_indent ('''
+...     First line.
+...     Second line.
+...     Third line.
+...         Fourth line (indented).
+...         Fifth line (ditto).
+...     Sixth line.
+... ''')
+First line.
+Second line.
+Third line.
+    Fourth line (indented).
+    Fifth line (ditto).
+Sixth line.
+>>> print normalized_indent ('''
+...         First line.
+...     Second line.
+...     Third line.
+...         Fourth line (indented).
+...         Fifth line (ditto).
+...     Sixth line.
+...
+... ''')
+First line.
+Second line.
+Third line.
+    Fourth line (indented).
+    Fifth line (ditto).
+Sixth line.
+>>> print normalized_indent ('''
+...  First line.
+...     Second line.
+...     Third line.
+...         Fourth line (indented).
+...         Fifth line (ditto).
+...     Sixth line.
+...
+... ''')
+First line.
+Second line.
+Third line.
+    Fourth line (indented).
+    Fifth line (ditto).
+Sixth line.
+"""
+
 from   _TFL import TFL
 
-import sys
-
-def normalized_indent (text) :
-    """Returns `text` with normalized indentation.
-
-       >>> normalized_indent ("Just a single line.")
-       'Just a single line.'
-       >>> normalized_indent ("  Just a single line inside white space.  ")
-       'Just a single line inside white space.'
-       >>> print normalized_indent ('''
-       ...     First line.
-       ...     Second line.
-       ...     Third line.
-       ...         Fourth line (indented).
-       ...         Fifth line (ditto).
-       ...     Sixth line.
-       ... ''')
-       First line.
-       Second line.
-       Third line.
-           Fourth line (indented).
-           Fifth line (ditto).
-       Sixth line.
-       >>> print normalized_indent ('''
-       ...         First line.
-       ...     Second line.
-       ...     Third line.
-       ...         Fourth line (indented).
-       ...         Fifth line (ditto).
-       ...     Sixth line.
-       ...
-       ... ''')
-       First line.
-       Second line.
-       Third line.
-           Fourth line (indented).
-           Fifth line (ditto).
-       Sixth line.
-       >>> print normalized_indent ('''
-       ...  First line.
-       ...     Second line.
-       ...     Third line.
-       ...         Fourth line (indented).
-       ...         Fifth line (ditto).
-       ...     Sixth line.
-       ...
-       ... ''')
-       First line.
-       Second line.
-       Third line.
-           Fourth line (indented).
-           Fifth line (ditto).
-       Sixth line.
-       >>>
-    """
+def _normalized_indent (text) :
+    """Returns `text` with normalized indentation."""
     lines = text.strip ().split ("\n")
     head  = lines [0]
     rest  = lines [1:]
@@ -100,8 +114,23 @@ def normalized_indent (text) :
         if indent :
             lines = [head] + [line [indent:] for line in rest]
     return "\n".join (lines)
-# end def normalized_indent
+# end def _normalized_indent
+
+try :
+    from textwrap import dedent as _dedent
+except ImportError :
+    normalized_indent = _normalized_indent
+else :
+    def normalized_indent (text) :
+        """Returns `text` with normalized indentation."""
+        lines  = text.strip ().split ("\n", 1)
+        result = lines [0]
+        rest   = lines [1:]
+        if rest :
+            result = "\n".join ((result, _dedent (rest [0])))
+        return result
+    # end def normalized_indent
 
 if __name__ != "__main__" :
-    TFL._Export ("*")
+    TFL._Export ("normalized_indent")
 ### __END__ normalized_indent
