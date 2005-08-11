28cd8720b8025901234969e3013cbead96acd492
Philip Gortan <gortan@gmail.com>
2005-08-11 14:54:33 +0000

    Recent changes

--- _TFL/Traceback_Printer.py
+++ _TFL/Traceback_Printer.py
@@ -1,6 +1,21 @@
 # -*- coding: iso-8859-1 -*-
-# Copyright (C) 2005 TTTech Computertechnik AG. All rights reserved.
-# Schoenbrunnerstrasse 7, 1040 Wien, Austria. office@tttech.com
+# Copyright (C) 2005  Philipp Gortan <gortan@tttech.com>
+# ****************************************************************************
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# ****************************************************************************
 #
 #++
 # Name
