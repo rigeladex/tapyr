3355a0cc0c67b518afbc72e696c50c8890f55fb8
Christian Tanzer <tanzer@swing.co.at>
2006-02-28 10:06:31 +0000

    Recent changes

--- _TFL/Extreme_Value_Counter.py
+++ _TFL/Extreme_Value_Counter.py
@@ -1,6 +1,22 @@
 # -*- coding: iso-8859-1 -*-
 # Copyright (C) 2004 TTTech Computertechnik AG. All rights reserved
 # Schönbrunnerstraße 7, A--1040 Wien, Austria. office@tttech.com
+# ****************************************************************************
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the Free
+# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# ****************************************************************************
 #
 #++
 # Name
--- _TFL/Gauge_Logger.py
+++ _TFL/Gauge_Logger.py
@@ -1,6 +1,22 @@
 # -*- coding: iso-8859-1 -*-
-# Copyright (C) 2000-2003 TTTech Computertechnik GmbH. All rights reserved
-# Schönbrunnerstraße 7, A--1040 Wien, Austria. office@tttech.com
+# Copyright (C) 2000-2005 Mag. Christian Tanzer. All rights reserved
+# Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
+# ****************************************************************************
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the Free
+# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# ****************************************************************************
 #
 #++
 # Name
@@ -12,22 +28,23 @@
 #    interactive and batch applications)
 #
 # Revision Dates
-#    27-Jan-2000 (MG) Creation
-#     7-Feb-2000 (CT) `__nonzero__' added
-#    29-Jun-2000 (CT) Use `predicate.relax' instead of home-grown `_gobble'
-#    14-Jul-2000 (MG) Methods `__str__' and `__repr__' added
-#    18-Jul-2000 (GS) Corrected a typo in __repr__ (selg -> self)
-#    27-Jul-2000 (CT) `verbose' added
-#    11-Jun-2003 (CT) s/!= None/is not None/
+#    27-Jan-2000 (MG)  Creation
+#     7-Feb-2000 (CT)  `__nonzero__' added
+#    29-Jun-2000 (CT)  Use `predicate.relax' instead of home-grown `_gobble'
+#    14-Jul-2000 (MG)  Methods `__str__' and `__repr__' added
+#    18-Jul-2000 (GS)  Corrected a typo in __repr__ (selg -> self)
+#    27-Jul-2000 (CT)  `verbose' added
+#    11-Jun-2003 (CT)  s/!= None/is not None/
 #    31-Mar-2004 (GWA) 'Gauge_Interval', 'Gauge_Handler' added
 #     6-May-2004 (GWA) 'Gauge_Handler' improved
 #    10-May-2004 (GWA) '__getattr__' fixed in 'Gauge_Handler'
 #    14-Mar-2005 (CT)  `_activate` added
 #    14-Mar-2005 (CT)  Dead code courtesy of GWA removed
-#    21-Jan-2006 (MG) Moved into `TFL` package
+#    21-Jan-2006 (MG)  Moved into `TFL` package
 #    ««revision-date»»···
 #--
 
+from _TFL           import TFL
 from _TFL.predicate import relax
 
 class Gauge_Logger :
@@ -75,6 +92,5 @@ class Gauge_Logger :
 # end class Gauge_Logger
 
 if __name__ != "__main__" :
-    from _TFL import TFL
     TFL._Export ("*")
 ### __END__ TFL.Gauge_Logger
