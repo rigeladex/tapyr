4b176ed2418aa2be06802aa46105dc002e90c830
Christian Tanzer <tanzer@swing.co.at>
2006-07-05 07:49:21 +0000

    Recent changes

--- _ATAX/kassabuch.py
+++ _ATAX/kassabuch.py
@@ -1,19 +1,18 @@
-#!/swing/bin/python
 # -*- coding: iso-8859-1 -*-
-# Copyright (C) 1999 Mag. Christian Tanzer. All rights reserved
+# Copyright (C) 1999-2006 Mag. Christian Tanzer. All rights reserved
 # Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
 # ****************************************************************************
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -29,12 +28,14 @@
 # Revision Dates
 #     9-Feb-1999 (CT) Creation
 #     6-Feb-2000 (CT) `vst_korrektur' passed to `V_Account' instead of to
-#                     `umsatzsteuer' 
+#                     `umsatzsteuer'
+#     5-Jul-2006 (CT) Import from `_ATAX`
 #    ««revision-date»»···
 #--
 
-from accounting   import *
-from umsatzsteuer import umsatzsteuer
+from _ATAX.accounting   import *
+from _ATAX.umsatzsteuer import umsatzsteuer
+
 EUC  = EU_Currency
 
 par_sub_pat = re.compile ("\(([^)]+)\)")
@@ -45,22 +46,25 @@ def kassabuch (account, file) :
         if not isinstance (e, Account_Entry) : continue
         e.desc = par_sub_pat.sub ("[\\1]", e.desc)
         if e.dir == "-" :
-            file.write ( "\\A %6s %-50s (%8s) [%s]\n"
-                       % (e.date, e.desc, e.gross.as_string (), e.number)
-                       )
+            file.write \
+                ( "\\A %6s %-50s (%8s) [%s]\n"
+                % (e.date, e.desc, e.gross.as_string (), e.number)
+                )
         elif e.dir == "+" :
             print "Income entries not yet implemented", e
 # end def kassabuch
 
 if __name__ == "__main__":
-    from Command_Line   import Command_Line
-    from sys            import stdin, stdout
-    cmd = Command_Line  ( option_spec = ( "-source_currency:S=eur#2"
-                                        , "-target_currency:S=eur"
-                                        , "-vst_korrektur:F=1.0"
-                                        , "-output:S"
-                                        )
-                        )
+    from _TFL.Command_Line   import Command_Line
+    from sys                 import stdin, stdout
+    cmd = Command_Line  \
+        ( option_spec =
+            ( "-source_currency:S=eur#2"
+            , "-target_currency:S=eur"
+            , "-vst_korrektur:F=1.0"
+            , "-output:S"
+            )
+        )
     vst_korrektur       = cmd.option ["vst_korrektur"].value_1 ()
     categories          = "[u]"
     source_currency     = EUC.Table [cmd.option ["source_currency"].value_1 ()]
@@ -68,7 +72,7 @@ if __name__ == "__main__":
     account             = V_Account (vst_korrektur = vst_korrektur)
     if cmd.argn > 0 :
         for file_name in cmd.argv.body :
-            file = open (file_name, "r")
+            file = open  (file_name, "r")
             umsatzsteuer (file, categories, source_currency, account)
     else :
         umsatzsteuer     (stdin, categories, source_currency, account)
