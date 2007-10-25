5b3a2bed088c3d45a4775294433e3c3cd234d7b2
Dr. Ralf Schlatterbeck <rsc@runtux.com>
2007-10-25 10:28:02 +0000

    Add setup.py -- initial attempt at creating a distribution.

---
+++ _TFL/setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 1998-2007 Mag. Christian Tanzer. All rights reserved
+# Glasauergasse 32, A--1130 Wien, Austria.
+# Web: http://www.c-tanzer.at/en/ Email: tanzer@swing.co.at
+# All rights reserved
+# ****************************************************************************
+#
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Library General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# ****************************************************************************
+
+import os
+from distutils.core import setup
+
+name    = 'TFL'
+license = 'GNU Library or Lesser General Public License (LGPL)'
+VERSION = os.popen ('svnversion').read ().strip ()
+pkgs    = []
+# path-walking probably only works in Unix due to hardcoded '/'
+for root, dirs, files in os.walk ('../_' + name) :
+    # ugly: need to modify dirs in place!
+    dd = dict.fromkeys (d for d in dirs if d.startswith ('_'))
+    for d in dirs :
+        if d not in dd :
+            dirs.remove (d)
+    pkgs.append (root.replace ('/', '.') [3:])
+
+setup \
+    ( name             = name
+    , version          = VERSION
+    , description      = "Library with lots of useful stuff"
+    # long_description = 
+    , license          = license
+    , author           = "Christian Tanzer and others"
+    , author_email     = "tanzer@swing.co.at"
+    # url              = "http://"
+    , packages         = pkgs
+    , package_dir      = {'_' + name : ''}
+    , platforms        = 'Any'
+    , classifiers      = \
+        [ 'Development Status :: 6 - Mature'
+        , 'License :: OSI Approved :: ' + license
+        , 'Operating System :: OS Independent'
+        , 'Programming Language :: Python'
+        , 'Intended Audience :: Developers'
+        , 'Topic :: Software Development :: Libraries'
+        , 'Topic :: Software Development :: Libraries :: Application Frameworks'
+        , 'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+    )
+
