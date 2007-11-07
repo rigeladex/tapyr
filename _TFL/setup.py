1d39d6eb9d96dc95aad9e7efa1d335a20d6f998e
Christian Tanzer <tanzer@swing.co.at>
2007-11-07 11:30:08 +0000

    Recent changes

--- _TFL/setup.py
+++ _TFL/setup.py
@@ -54,6 +54,7 @@ if version.isdigit () :
         , url              = "http://www.c-tanzer.at/en"
         , packages         = pkgs
         , package_dir      = {'_' + name : ''}
+        , data_files       = ["LGPL"]
         , platforms        = 'Any'
         , classifiers      = \
             [ 'Development Status :: 6 - Mature'
