57a56c0c50170254eeffa45e2b6164987e2d4e61
Christian Tanzer <tanzer@swing.co.at>
2009-05-12 08:44:28 +0000

    Recent changes

---
+++ _LNX/cpu_info.py
@@ -0,0 +1,132 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2009 Mag. Christian Tanzer. All rights reserved
+# Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
+# ****************************************************************************
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the Free
+# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# ****************************************************************************
+#
+#++
+# Name
+#    LNX.cpu_info
+#
+# Purpose
+#    Provide information about CPU a Linux system is running on
+#
+# Revision Dates
+#    12-May-2009 (CT) Creation
+#    ««revision-date»»···
+#--
+
+from   __future__  import with_statement
+
+from   _LNX import LNX
+from   _TFL import TFL
+
+import _TFL._Meta.Object
+from   _TFL.Regexp import *
+
+class _Cpu_Info_ (TFL.Meta.Object) :
+
+    _dict = None
+    _keys = set ()
+
+    _paren_rep  = Re_Replacer (r"\s*\([^)]*\)\s*", " ")
+    _ws_rep     = Re_Replacer (r"\s\s+",           " ")
+    _ignore_rep = Re_Replacer (r"Intel\s+|CPU.*",  "")
+    _space_rep  = Re_Replacer (r" ", "_")
+
+    def items (self) :
+        return list (self.iteritems ())
+    # end def items
+
+    def iteritems (self) :
+        map  = self._dict
+        for k in self.iterkeys () :
+            yield k, map [k]
+    # end def iteritems
+
+    def iterkeys (self) :
+        if self._dict is None :
+            self._setup ()
+        return iter (self._keys)
+    # end def iterkeys
+
+    def itervalues (self) :
+        for k, v in self.iteritems () :
+            yield v
+    # end def itervalues
+
+    def keys (self) :
+        return list (self.iterkeys ())
+    # end def keys
+
+    def values (self) :
+        return list (self.itervalues ())
+    # end def values
+
+    def _setup (self) :
+        map  = self._dict = {}
+        keys = self._keys
+        with open ("/proc/cpuinfo") as f :
+            for l in f :
+                l = l.strip ()
+                if not l :
+                    continue
+                k, v  = l.split (":")
+                k     = k.strip ()
+                if k not in keys :
+                    v = v.strip ()
+                    if k == "model name" :
+                        v = self._paren_rep  (v)
+                        v = self._ignore_rep (v)
+                    if k == "cpu MHz" :
+                        try :
+                            v = float (v)
+                        except :
+                            pass
+                        else :
+                            v = "%5.0f" % (v, )
+                    v = self._ws_rep (v).strip ()
+                    map [k] = v
+                    keys.add (k)
+                    kk = self._space_rep (k)
+                    if kk != k :
+                        map [kk] = v
+    # end def _setup
+
+    def __getattr__ (self, name) :
+        if self._dict is None :
+            self._setup ()
+        result = self._dict [name]
+        setattr (self, name, result)
+        return result
+    # end def __getattr__
+
+    def __getitem__ (self, key) :
+        if self._dict is None :
+            self._setup ()
+        return self._dict [key]
+    # end def __getitem__
+
+# end class _Cpu_Info_
+
+cpu_info = _Cpu_Info_ ()
+
+if __name__ != "__main__" :
+    LNX._Export ("cpu_info")
+else :
+    print "%(model name)s/%(cpu MHz)s" % cpu_info
+### __END__ LNX.cpu_info
