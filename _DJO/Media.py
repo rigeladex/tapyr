321f28c74c5e0038a45cfeabfd914096ac84a615
Christian Tanzer <tanzer@swing.co.at>
2009-07-10 10:05:50 +0000

    Recent changes

--- _DJO/Media.py
+++ _DJO/Media.py
@@ -27,12 +27,14 @@
 #
 # Revision Dates
 #     9-Jul-2009 (CT) Creation
+#    10-Jul-2009 (CT) Creation continued
 #    ««revision-date»»···
 #--
 
 from   _TFL                               import TFL
 from   _DJO                               import DJO
 
+import _TFL.predicate
 import _TFL._Meta.Object
 from   _TFL._Meta.Once_Property           import Once_Property
 
@@ -58,85 +60,147 @@ class CSS_Link (TFL.Meta.Object) :
         return hash ((self.href, self.media_type))
     # end def __hash__
 
+    def __repr__ (self) :
+        return "%s: %s" % (self.media_type, self.href)
+    # end def __repr__
+
+    def __str__ (self) :
+        return self.href
+    # end def __str__
+
 # end class CSS_Link
 
-class Media (TFL.Meta.Object) :
+class Media_List (TFL.Meta.Object) :
+    """Model a list of media objects"""
 
-    css_prefix = "styles"
-    js_prefix  = "js"
-
-    @Once_Property
-    def media_url (self) :
-        from django.conf import settings
-        return settings.MEDIA_URL
-    # end def media_url
-
-    def __init__ (self, css_links = (), js_links = (), js_on_ready = (), js_code = (), children = ()) :
-        self._css_links   = tuple (self._sanitized_css_links (css_links))
-        self._js_links    = js_links
-        self._js_on_ready = js_on_ready
-        self._js_code     = js_code
-        self.children     = list (children)
+    def __init__ (self, name, media, mobs) :
+        self.name  = name
+        self.media = media
+        self.mobs  = mobs
     # end def __init__
 
     @Once_Property
-    def css_links (self) :
-        return tuple (self._filter (self._gen ("css_link", self._css_massage)))
-    # end def css_links
+    def values (self) :
+        return tuple (self._gen_all ())
+    # end def values
 
-    @Once_Property
-    def js_code (self) :
-        return tuple (self._gen ("js_code"))
-    # end def js_code
-
-    @Once_Property
-    def js_links (self) :
-        return tuple (self._filter (self._gen ("js_link", self._js_massage)))
-    # end def js_links
-
-    @Once_Property
-    def js_on_ready (self) :
-        return tuple (self._gen ("js_on_ready"))
-    # end def js_on_ready
-
-    def _css_massage (self, l) :
-        if not l.href.startswith (("http://", "https://", "/")) :
-            l.href = pjoin (self.media_url, self.css_prefix, l.href)
-        return l
-    # end def _css_link_gen
-
-    def _filter (links) :
-        seen = set ()
-        for l in links :
-            if l not in seen :
+    def _gen_all (self) :
+        for mob in self._gen_own () :
+            yield mob
+        name = self.name
+        for c in self.media.children :
+            for l in getattr (c, name) :
                 yield l
-                seen.add (l)
-    # end def _filter
+    # end def _gen_all
 
-    def _gen (self, attr, massage = None) :
-        _attr = "_" + attr
-        for l in getattr (self, _attr) :
-            if massage :
-                l = massage (l)
-            yield l
-        for c in self.children :
-            for l in getattr (c, attr) :
-                yield l
-    # end def _js_code_gen
+    def _gen_own (self) :
+        for mob in self.mobs :
+            yield mob
+    # end def _gen_own
 
-    def _js_massage (self, l) :
-        if not l.startswith (("http://", "https://", "/")) :
-            l = pjoin (self.media_url, self.js_prefix, l)
-        return l
-    # end def _js_massage
+    def __iter__ (self) :
+        return iter (self.values)
+    # end def __iter__
 
-    def _sanitized_css_links (self, css_links) :
-        for l in css_links :
-            if not isinstance (l, CSS_Link) :
-                l = CSS_Link (l)
-            yield l
-    # end def _sanitized_css_links
+# end class Media_List
+
+class Media_List_Unique (Media_List) :
+    """Model a list of unique media objects (filters duplicates)."""
+
+    def _gen_all (self) :
+        return TFL.uniq (self.__super._gen_all ())
+    # end def _gen_all
+
+# end class Media_List_Unique
+
+class Media_List_CSSL (Media_List_Unique) :
+    """Model a list of CSS_Link objects"""
+
+    prefix = "styles"
+
+    def __init__ (self, name, media, mobs) :
+        self.__super.__init__ (name, media, tuple (self._sanitized (mobs)))
+    # end def __init__
+
+    def _gen_own (self) :
+        prefix = self.prefix
+        url    = self.media.url
+        for mob in self.__super._gen_own () :
+            if not mob.href.startswith (("http://", "https://", "/")) :
+                mob.href = pjoin (url, prefix, mob.href)
+            yield mob
+    # end def _gen_own
+
+    def _sanitized (self, mobs) :
+        for mob in mobs :
+            if not isinstance (mob, CSS_Link) :
+                mob = CSS_Link (mob)
+            yield mob
+    # end def _sanitized
+
+# end class Media_List_CSSL
+
+class Media_List_JSL (Media_List_Unique) :
+    """Model a list of Javascript link objects"""
+
+    prefix = "js"
+
+    def _gen_own (self) :
+        prefix = self.prefix
+        url    = self.media.url
+        for mob in self.__super._gen_own () :
+            if not mob.startswith (("http://", "https://", "/")) :
+                mob = pjoin (url, prefix, mob)
+            yield mob
+    # end def _gen_own
+
+# end class Media_List_JSL
+
+class Media (TFL.Meta.Object) :
+    """
+       >>> class Media (Media) :
+       ...     url = "/test/"
+       ...
+       >>> NL = chr (10)
+
+       >>> m = Media (css_links = ("a.css", "/b/c.css"),
+       ...       js_links = ("foo.js", "bar.js", "http://baz.js"))
+       >>> print NL.join (repr (l) for l in m.css_links)
+       all: /test/styles/a.css
+       all: /b/c.css
+       >>> tuple (m.js_links)
+       ('/test/js/foo.js', '/test/js/bar.js', 'http://baz.js')
+       >>> n = Media (("/test/styles/a.css", CSS_Link ("c.css", "screen")))
+       >>> print NL.join (repr (l) for l in n.css_links)
+       all: /test/styles/a.css
+       screen: /test/styles/c.css
+       >>> tuple (n.js_links)
+       ()
+       >>> q = Media (js_links = ("qux.js", ), children = (m, n))
+       >>> print NL.join (repr (l) for l in q.css_links)
+       all: /test/styles/a.css
+       all: /b/c.css
+       screen: /test/styles/c.css
+       >>> "; ".join (q.js_links)
+       '/test/js/qux.js; /test/js/foo.js; /test/js/bar.js; http://baz.js'
+    """
+
+    @Once_Property
+    def url (self) :
+        from django.conf import settings
+        return settings.MEDIA_URL
+    # end def url
+
+    def __init__ (self, css_links = (), js_links = (), js_on_ready = (), js_code = (), children = ()) :
+        self.css_links   = Media_List_CSSL ("css_links",   self, css_links)
+        self.js_links    = Media_List_JSL  ("js_links",    self, js_links)
+        self.js_on_ready = Media_List      ("js_on_ready", self, js_on_ready)
+        self.js_code     = Media_List      ("js_code",     self, js_code)
+        self.children    = list            (children)
+    # end def __init__
 
 # end class Media
 
+if __name__ != "__main__":
+    DJO._Export ("*")
 ### __END__ DJO.Media
