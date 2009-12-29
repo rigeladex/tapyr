5e87a1ccef12e027b095a16dc0b61f20e7042290
Christian Tanzer <tanzer@swing.co.at>
2009-12-29 10:02:08 +0000

    Recent changes

--- _JNJ/Environment.py
+++ _JNJ/Environment.py
@@ -34,32 +34,149 @@ from   _JNJ               import JNJ
 from   _TFL               import TFL
 
 import _JNJ.Onion
+import _JNJ.GTW
 
+from   _TFL               import sos
 from   _TFL.predicate     import uniq
 
-from   jinja2             import Environment, FileSystemLoader
+from   jinja2             import Environment, FileSystemLoader, ChoiceLoader
 
 def HTML (version = "html/5.jnj", load_path = (), loader = None, globals = {}, translations = None, ** kw) :
     if load_path :
         assert loader is None
         encoding = kw.pop ("encoding", "iso-8859-1")
         loader   = FileSystemLoader (load_path, encoding)
+    jnj_loader   = FileSystemLoader (sos.path.dirname (__file__), "iso-8859-1")
+    if loader is None :
+        loader   = jnj_loader
+    else :
+        loader   = ChoiceLoader (loader, jnj_loader)
     extensions   = uniq \
         ( kw.pop ("extensions", [])
         + ["jinja2.ext.loopcontrols", "jinja2.ext.i18n", "jinja2.ext.do"]
         + [JNJ.Onion]
         )
     result = JNJ.Environment.active = Environment \
-        ( extensions = extensions
-        , loader     = loader
+        ( extensions   = extensions
+        , loader       = loader
         , ** kw
         )
-    result.globals.update (globals)
-    result.globals ["html_version"] = version
+    result.globals.update \
+        ( globals
+        , GTW          = JNJ.GTW
+        , html_version = version
+        )
     if translations is not None :
         env.install_gettext_translations (translations)
     return result
 # end def HTML
+
+__doc__ = """
+`HTML` creates an environment suitable for generating HTML from jinja
+templates. Per default, `HTML` sets `html_version` to a macro template for
+HTML5.
+
+    >>> env5     = HTML ()
+
+Passing `version = "html/x.jnj"` to `HTML` sets up an environment with
+`html_version` referring to a macro template for XHTML.
+
+    >>> envx     = HTML (version = "html/x.jnj")
+
+By import `html_version` as `x` and calling macros defined by `X`, a template
+can generate either HTML5 or XHTML output, depending on how the environment
+was set up::
+
+    >>> template = '''
+    ... {%- import html_version as X -%}
+    ... {%- call X.html (lang = "de") -%}
+    ...   {%- call X.section (id = "foo", class = "bar") -%}
+    ...     A sample section for testing the differences between
+    ...     html5 and xhtml.
+    ...   {%- endcall -%}
+    ...
+    ...   {%- set a = None %}
+    ...   {%- set b = 42 %}
+    ...   {%- set c = "cccc" %}
+    ...   {{ GTW.firstof (a, b, c, d, "default first of") }}
+    ...   {{ GTW.firstof (b, c, d, "default first of") }}
+    ...   {{ GTW.firstof (c, d, "default first of") }}
+    ...   {{ GTW.firstof (d, "default first of") }}
+    ...   {{ GTW.firstof ("default first of") }}
+    ... {%- endcall -%}
+    ... '''
+    >>> t5 = env5.from_string (template)
+    >>> tx = envx.from_string (template)
+    >>> print t5.render ()
+    <!DOCTYPE html>
+    <html lang="de">
+    <BLANKLINE>
+    <section  id="foo" class="bar">
+      A sample section for testing the differences between
+        html5 and xhtml.
+    </section>
+    <BLANKLINE>
+      42
+      42
+      cccc
+      default first of
+      default first of
+    </html>
+    <BLANKLINE>
+    >>> print tx.render ()
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"
+    >
+    <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de" >
+    <BLANKLINE>
+    <div class="section bar"  id="foo">
+      A sample section for testing the differences between
+        html5 and xhtml.
+    </div>
+    <BLANKLINE>
+      42
+      42
+      cccc
+      default first of
+      default first of
+    </html>
+    <BLANKLINE>
+    >>> print t5.render (d = "qux")
+    <!DOCTYPE html>
+    <html lang="de">
+    <BLANKLINE>
+    <section  id="foo" class="bar">
+      A sample section for testing the differences between
+        html5 and xhtml.
+    </section>
+    <BLANKLINE>
+      42
+      42
+      cccc
+      qux
+      default first of
+    </html>
+    <BLANKLINE>
+    >>> print tx.render (d = "qux")
+    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"
+    >
+    <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de" lang="de" >
+    <BLANKLINE>
+    <div class="section bar"  id="foo">
+      A sample section for testing the differences between
+        html5 and xhtml.
+    </div>
+    <BLANKLINE>
+      42
+      42
+      cccc
+      qux
+      default first of
+    </html>
+    <BLANKLINE>
+
+"""
 
 if __name__ != "__main__" :
     JNJ._Export_Module ()
