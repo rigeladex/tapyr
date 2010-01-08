7d575ca68a536c1cc2d1daf261d338a6df85e442
Christian Tanzer <tanzer@swing.co.at>
2010-01-08 10:20:17 +0000

    Recent changes

--- _GTW/_NAV/__init__.py
+++ _GTW/_NAV/__init__.py
@@ -1,41 +1,42 @@
 # -*- coding: iso-8859-1 -*-
-# Copyright (C) 2007-2008 Mag. Christian Tanzer. All rights reserved
+# Copyright (C) 2007-2010 Mag. Christian Tanzer. All rights reserved
 # Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
 # ****************************************************************************
+# This package is part of the package GTW.
 #
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Library General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
+# This package is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# This library is distributed in the hope that it will be useful,
+# This package is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Library General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU Library General Public
-# License along with this library; if not, write to the Free
-# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# You should have received a copy of the GNU Affero General Public License
+# along with this package. If not, see <http://www.gnu.org/licenses/>.
 # ****************************************************************************
 #
 #++
 # Name
-#    DJO.NAV.__init__
+#    GTW.NAV.__init__
 #
 # Purpose
-#    Package with Django specific classes and functions for navigation
+#    Model navigation on websites
 #
 # Revision Dates
 #    18-Oct-2008 (CT) Creation (factored from DJO.Navigation)
+#     8-Jan-2010 (CT) Moved from DJO to GTW
 #    ««revision-date»»···
 #--
 
 from   _TFL.Package_Namespace import Package_Namespace
-from   _DJO                   import DJO
+from   _GTW                   import GTW
 
 NAV = Package_Namespace ()
-DJO._Export ("NAV")
+GTW._Export ("NAV")
 
 del Package_Namespace
 
-### __END__ DJO.NAV.__init__
+### __END__ GTW.NAV.__init__
