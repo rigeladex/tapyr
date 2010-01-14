53c1c64633f338e2b760ec97ce057889452c9035
Martin Glueck <martin@mangari.org>
2010-01-14 12:35:29 +0000

    Next try with new tools

--- _GTW/_Auth/__init__.py
+++ _GTW/_Auth/__init__.py
@@ -1,1 +1,41 @@
-# -*- coding: iso-8859-1 -*-# Copyright (C) 2010 Martin Glück. All rights reserved# Langstrasse 4, A--2244 Spannberg, Austria. martin@mangari.org# ****************************************************************************# This package is part of the package GTW.## This package is free software: you can redistribute it and/or modify# it under the terms of the GNU Affero General Public License as published by# the Free Software Foundation, either version 3 of the License, or# (at your option) any later version.## This package is distributed in the hope that it will be useful,# but WITHOUT ANY WARRANTY; without even the implied warranty of# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the# GNU Affero General Public License for more details.## You should have received a copy of the GNU Affero General Public License# along with this package. If not, see <http://www.gnu.org/licenses/>.# ****************************************************************************##++# Name#    GTW.Auth.__init__## Purpose#    Support for authentication## Revision Dates#    13-Jan-2010 (MG) Creation#    ««revision-date»»···#--from   _TFL.Package_Namespace import Package_Namespacefrom   _GTW                   import GTWAuth = Package_Namespace ()GTW._Export ("Auth")del Package_Namespace### __END__ GTW.Auth.__init__
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2010 Martin Glück. All rights reserved
+# Langstrasse 4, A--2244 Spannberg, Austria. martin@mangari.org
+# ****************************************************************************
+# This package is part of the package GTW.
+#
+# This package is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package. If not, see <http://www.gnu.org/licenses/>.
+# ****************************************************************************
+#
+#++
+# Name
+#    GTW.Auth.__init__
+#
+# Purpose
+#    Support for authentication
+#
+# Revision Dates
+#    13-Jan-2010 (MG) Creation
+#    ««revision-date»»···
+#--
+
+from   _TFL.Package_Namespace import Package_Namespace
+from   _GTW                   import GTW
+
+Auth = Package_Namespace ()
+GTW._Export ("Auth")
+
+del Package_Namespace
+
+### __END__ GTW.Auth.__init__
