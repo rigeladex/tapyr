8e793edef8168a908db6c2631db7240137a00e4f
Martin Glueck <martin@mangari.org>
2010-04-19 21:03:29 +0000

    Test directory started

---
+++ _GTW/__test__/MOM.py
@@ -0,0 +1,44 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2010 Martin Glueck All rights reserved
+# Langstrasse 4, A--2244 Spannberg, Austria. martin@mangari.org
+# ****************************************************************************
+# This module is part of the package GTW.__test__.
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This module is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this module. If not, see <http://www.gnu.org/licenses/>.
+# ****************************************************************************
+#
+#++
+# Name
+#    GTW.__test__.MOM
+#
+# Purpose
+#    Proxy for the MOM.__doc__ tests.
+#
+# Revision Dates
+#    19-Apr-2010 (MG) Creation
+#    ««revision-date»»···
+#--
+"""
+This is just to make sure that run_doctest considers this file as a file with
+doctests
+    >>> Print
+"""
+from _MOM.__doc__ import __doc__, MOM, BMT, show, NL, sos
+
+__doc__ = __doc__
+
+if __name__ == "__main__" :
+    import doctest
+    doctest.testmod ()
+### __END__ GTW.__test__.MOM
