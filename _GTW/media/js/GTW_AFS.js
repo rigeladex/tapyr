9e56756b3af591e5457145396a86e062b9c24490
Christian Tanzer <tanzer@swing.co.at>
2011-01-18 14:47:10 +0000

    Recent changes

---
+++ _GTW/media/js/GTW_AFS.js
@@ -0,0 +1,45 @@
+/*
+** Copyright (C) 2011 Mag. Christian Tanzer All rights reserved
+** Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
+** ****************************************************************************
+** This file is free software: you can redistribute it and/or modify
+** it under the terms of the GNU Affero General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This file is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU Affero General Public License for more details.
+**
+** You should have received a copy of the GNU Affero General Public License
+** along with this file. If not, see <http://www.gnu.org/licenses/>.
+** ****************************************************************************
+**
+**++
+** Name
+**    GTW_AFS
+**
+** Purpose
+**    jQuery plugin for AJAX-enhanced forms
+**
+**
+** Revision Dates
+**    18-Jan-2011 (CT) Creation
+**    ««revision-date»»···
+**--
+*/
+
+( function($)
+  {
+    $.fn.gtw_afs = function (options)
+      {
+        ««js-statement»»···
+      }
+    ««js-statement»»···;
+  }
+) (jQuery);
+
+/* __END__ GTW_AFS.js */
+
+
