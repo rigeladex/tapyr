eef83135e5ca5b7c9ef1df96f350a00d6aeb2413
Christian Tanzer <tanzer@swing.co.at>
2011-03-10 17:42:22 +0000

    Recent changes

---
+++ _GTW/media/js/GTW/Set.js
@@ -0,0 +1,148 @@
+// Copyright (C) 2011 Mag. Christian Tanzer All rights reserved
+// Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
+// #*** <License> ************************************************************#
+// This script is licensed under the terms of either the
+// MIT License or the GNU Affero General Public License (AGPL) Version 3.
+// http://www.c-tanzer.at/license/mit_or_agpl.html
+// #*** </License> ***********************************************************#
+//
+//++
+// Name
+//    GTW/Set
+//
+// Purpose
+//    Provide a javascript class modelling a Set, i.e., an unordered set of
+//    unique elements
+//
+// Revision Dates
+//    10-Mar-2011 (CT) Creation
+//    ««revision-date»»···
+//--
+
+( function () {
+    var _ = {}, hasOwnProperty = _.hasOwnProperty, token = {};
+    var Set = $GTW.Class.extend (
+        { init : function init () {
+              this.update.apply (this, arguments);
+          }
+        , add : function add (arg) {
+              this [arg] = token;
+              return this;
+          }
+        , clear : function clear () {
+              var keys = this.elements ();
+              for (var i = 0, li = keys.length, k; i < li; i++) {
+                  k = keys [i];
+                  delete this [k];
+              }
+          }
+        , contains : function contains (arg) {
+              var elem = this [arg];
+              return elem && elem === token;
+          }
+        , copy : function copy () {
+              var result = new Set ();
+              result.union_update (this);
+              return result;
+          }
+        , difference : function difference (other) {
+              var result = this.copy ();
+              result.difference_update (other);
+              return result;
+          }
+        , difference_update : function difference_update (other) {
+              var keys = other.elements ();
+              for (var i = 0, li = keys.length, k; i < li; i++) {
+                  k = keys [i];
+                  if (this.contains (k)) {
+                      delete this [k];
+                  }
+              }
+              return this;
+          }
+        , discard : function discard (arg) {
+              delete this [arg];
+              return this;
+          }
+        , elements : function elements () {
+              return $GTW.inspect.keys
+                  ( this
+                  , function (name, obj) { return obj [name] === token; }
+                  );
+          }
+        , intersection : function intersection (other) {
+              var result = this.copy ();
+              result.intersection_update (other);
+              return result;
+          }
+        , intersection_update : function intersection_update (other) {
+              var keys = this.elements ();
+              for (var i = 0, li = keys.length, k; i < li; i++) {
+                  k = keys [i];
+                  if (! other.contains (k)) {
+                      delete this [k];
+                  }
+              }
+              return this;
+          }
+        , is_disjoint : function is_disjoint (other) {
+              return this.intersection (other).size () === 0;
+          }
+        , is_subset : function is_subset (other) {
+              var keys = this.elements ();
+              for (var i = 0, li = keys.length, k; i < li; i++) {
+                  k = keys [i];
+                  if (! other.contains (k)) {
+                      return false;
+                  }
+              }
+              return true;
+          }
+        , is_superset : function is_superset (other) {
+              return other.is_subset (this);
+          }
+        , remove : function remove (arg) {
+              if (this.contains (arg)) {
+                  delete this [arg];
+              } else {
+                  throw new Error
+                      ("Set '" + this.elements () + "' doesn't contain " + arg);
+              }
+              return this;
+          }
+        , size : function size () {
+              return this.elements ().length;
+          }
+        , symmetric_difference : function symmetric_difference (other) {
+              var result = this.copy ();
+              result.symmetric_difference_update (other);
+              return result;
+          }
+        , symmetric_difference_update : function symmetric_difference_update (other) {
+              var section = this.intersection (other);
+              this.union_update (other).difference_update (section);
+              return this;
+          }
+        , union : function union (other) {
+              var result = this.copy ();
+              result.union_update (other);
+              return result;
+          }
+        , union_update : function union_update (other) {
+              this.update.apply (this, other.elements ());
+              return this;
+          }
+        , update : function update () {
+              for (var i = 0, li = arguments.length, arg; i < li; i++) {
+                  arg = arguments [i];
+                  this [arg] = token;
+              }
+              return this;
+          }
+        }
+    );
+    $GTW.Set = Set;
+  } ()
+);
+
+// __END__ GTW/Set.js
