1ea70011ea6fdba2ae33de4edc0efd1f3e0cac19
Christian Tanzer <tanzer@swing.co.at>
2011-07-19 19:38:14 +0200

    Add TFL.User_Config

---
+++ _TFL/User_Config.py
@@ -0,0 +1,81 @@
+# -*- coding: iso-8859-15 -*-
+# Copyright (C) 2011 Mag. Christian Tanzer All rights reserved
+# Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
+# #*** <License> ************************************************************#
+# This module is part of the package TFL.
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This module is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this module. If not, see <http://www.gnu.org/licenses/>.
+# #*** </License> ***********************************************************#
+#
+#++
+# Name
+#    TFL.User_Config
+#
+# Purpose
+#    Provide thread-local user configuration
+#
+# Revision Dates
+#    19-Jul-2011 (CT) Creation
+#    ««revision-date»»···
+#--
+
+from   __future__  import unicode_literals
+
+from   _MOM        import MOM
+from   _TFL        import TFL
+
+import _TFL._Meta.Property
+import _TFL.Context
+
+from   dateutil    import tz
+import locale
+import threading
+import sys
+
+class User_Config (threading.local) :
+    """Provide thread-local user configuration."""
+
+    _initialzed          = False
+
+    file_system_encoding = sys.getfilesystemencoding ()
+    input_encoding       = locale.getpreferredencoding ()
+    language             = ""
+    output_encoding      = input_encoding
+    time_zone            = tz.tzutc ()
+    user                 = None
+
+    def __init__ (self, ** kw) :
+        if self._initialzed :
+            raise SystemError \
+                ( "TFL.User_Config must not be called more than "
+                  "once per thread"
+                )
+        self._initialzed = True
+        self.__dict__.update (kw)
+    # end def __init__
+
+    LET = TFL.Meta.Class_and_Instance_Method (TFL.Context.attr_let)
+
+    def set_default (self, name, value) :
+        """Set default of attribute `name` to `value`."""
+        setattr (self.__class__, name, value)
+    # end def set_default
+
+# end class User_Config
+
+user_config = User_Config ()
+
+if __name__ != "__main__" :
+    TFL._Export ("*")
+### __END__ TFL.User_Config
