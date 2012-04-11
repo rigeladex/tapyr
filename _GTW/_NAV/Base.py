rigeladex/tapyr

commit b14f2f8d57cd65cad8697064545a1c86c566002b

Replace `GTW.NAV._Dir_.rendered` by `_effective`, change `universal_view` to use `_effective` (to ensure that the permission of a page is checked when the directory url mapping to that page is visited).
Record changes of electric objects (to ensure that those are written into the
database -- an anonymous account instance sometimes wasn't).

_GTW/_NAV/Base.py
@@ -275,6 +275,8 @@
 #    29-Mar-2012 (CT) Rename `CSS_Parameters` to `Media_Parameters`
 #    29-Mar-2012 (CT) Change `allow_user` to return `False` if
 #                     `login_required` and `not user`
+#    29-Mar-2012 (CT) Replace `_Dir_.rendered` by `_effective`, change
+#                     `universal_view` to use `_effective`
 #    ««revision-date»»···
 #--
 
@@ -657,6 +659,11 @@ class _Site_Entity_ (TFL.Meta.Object) :
         if self.top.domain :
             return "webmaster@%s" % self.top.domain
     # end def webmaster
+
+    @Once_Property
+    def _effective (self) :
+        return self
+    # end def _effective
 
     def _formatted_attr (self, name) :
         v = getattr (self, name)
@@ -1053,22 +1060,6 @@ class _Dir_ (_Site_Entity_) :
         return result
     # end def new_sub_dir
 
-    def rendered (self, handler, template = None) :
-        dt = self.dir_template
-        if dt is None :
-            try :
-                page = first (self.own_links)
-            except IndexError :
-                pass
-            else :
-                handler.context.update \
-                    ( nav_page = page
-                    , page     = page
-                    )
-                return page.rendered (handler, template)
-        return self.__super.rendered (handler, template or dt)
-    # end def rendered
-
     def sub_dir_iter (self) :
         for owl in self.own_links :
             if isinstance (owl, Dir) :
@@ -1083,6 +1074,19 @@ class _Dir_ (_Site_Entity_) :
             for t in d.template_iter () :
                 yield t
     # end def template_iter
+
+    @property
+    def _effective (self) :
+        dt = self.dir_template
+        if dt is None :
+            try :
+                page = first (self.own_links)
+            except IndexError :
+                pass
+            else :
+                return page
+        return self
+    # end def _effective
 
     def _get_child (self, child, * grandchildren) :
         for owl in self.own_links :
@@ -1339,10 +1343,11 @@ class Root (_Dir_) :
     @classmethod
     def universal_view (cls, handler) :
         href = handler.request.path [1:]
-        user = handler.current_user
         page = cls.page_from_href (href)
         HTTP = cls.top.HTTP
         if page :
+            page = page._effective
+            user = handler.current_user
             if handler.request.method not in page.SUPPORTED_METHODS :
                 raise HTTP.Error_405 (valid_methods = page.SUPPORTED_METHODS)
             if page.login_required :

_GTW/_OMP/_Auth/Account.py
@@ -1,5 +1,5 @@
 # -*- coding: iso-8859-15 -*-
-# Copyright (C) 2010-2011 Martin Glueck All rights reserved
+# Copyright (C) 2010-2012 Martin Glueck All rights reserved
 # Langstrasse 4, A--2244 Spannberg. martin@mangari.org
 # ****************************************************************************
 # This package is part of the package GTW.
@@ -55,7 +55,7 @@
 #    ««revision-date»»···
 #--
 
-from   __future__            import unicode_literals
+from   __future__             import unicode_literals
 
 from   _MOM.import_MOM        import *
 from   _GTW                   import GTW
@@ -71,7 +71,7 @@ _Ancestor_Essence = MOM.Object
 class _Auth_Account_ (Auth.Entity, _Ancestor_Essence) :
     """Model an user account."""
 
-    _real_name = "Account"
+    _real_name  = "Account"
 
     is_partial  = True
     is_relevant = True
@@ -158,7 +158,6 @@ class Account_Anonymous (_Ancestor_Essence) :
         # end class superuser
 
     # end class _Attributes
-
 
 # end class Account_Anonymous
 

_MOM/Scope.py
@@ -89,6 +89,7 @@
 #    21-Mar-2011 (MG) `copy` assert fixed
 #     9-Sep-2011 (CT) Use `.E_Type` instead of `._etype`
 #    19-Jan-2012 (CT) Change `add` to consider `entity._home_scope`
+#    29-Mar-2012 (CT) Change `add` to call `record_change` for `electric`, too
 #    ««revision-date»»···
 #--
 
@@ -252,8 +253,7 @@ class Scope (TFL.Meta.Object) :
             entity.home_scope = self
             entity._finish__init__ ()
         self.ems.add (entity)
-        if not entity.electric :
-            self.record_change (MOM.SCM.Change.Create, entity)
+        self.record_change (MOM.SCM.Change.Create, entity)
     # end def add
 
     def add_from_pickle_cargo (self, type_name, cargo, pid) :

commit 8becfeb6cb3991123ec0d93c2bedcbe1832b299f

Fix rendering of GTW.NAV directory entries: change `_Dir_._effective` to return `page._effective`, re-add `_Dir_.rendered` to use `.dir_template`, if any.
Bugs were introduced with commit::

    b14f2f8 2012-03-29 15:58 Christian Tanzer

_GTW/_NAV/Base.py
@@ -281,6 +281,8 @@
 #                     `handler.request.user` there, not in `_view`
 #     5-Apr-2012 (CT) Move `_get_user` in here from `GTW.Request_Handler`
 #     5-Apr-2012 (CT) Change `_new_edit_session` to allow `anonymous_account`
+#    11-Apr-2012 (CT) Change `_Dir_._effective` to return `page._effective`
+#    11-Apr-2012 (CT) Re-add `_Dir_.rendered` to use `.dir_template`, if any
 #    ««revision-date»»···
 #--
 
@@ -1089,6 +1091,10 @@ class _Dir_ (_Site_Entity_) :
                 yield owl
     # end def sub_dir_iter
 
+    def rendered (self, handler, template = None) :
+        return self.__super.rendered (handler, template or self.dir_template)
+    # end def rendered
+
     def template_iter (self) :
         for t in self.template, self.dir_template :
             if t :
@@ -1107,7 +1113,7 @@ class _Dir_ (_Site_Entity_) :
             except IndexError :
                 pass
             else :
-                return page
+                return page._effective
         return self
     # end def _effective
 