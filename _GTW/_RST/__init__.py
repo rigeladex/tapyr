0484d1a4563b38281ac5e4ad2c2bb8c42b2c8b66
Christian Tanzer <tanzer@swing.co.at>
2012-06-06 18:32:57 +0200

    Start package GTW.RST.

---
+++ _GTW/_RST/__init__.py
@@ -0,0 +1,41 @@
+# -*- coding: iso-8859-15 -*-
+# Copyright (C) 2012 Mag. Christian Tanzer. All rights reserved
+# Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
+# ****************************************************************************
+# This package is part of the package GTW.
+#
+# This package is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this package. If not, see <http://www.gnu.org/licenses/>.
+# ****************************************************************************
+#
+#++
+# Name
+#    GTW.RST.__init__
+#
+# Purpose
+#    Framework for RESTful web services
+#
+# Revision Dates
+#     6-Jun-2012 (CT) Creation
+#    ««revision-date»»···
+#--
+
+from   _TFL.Package_Namespace import Package_Namespace
+from   _GTW                   import GTW
+
+RST = Package_Namespace ()
+GTW._Export ("RST")
+
+del Package_Namespace
+
+### __END__ GTW.RST.__init__
