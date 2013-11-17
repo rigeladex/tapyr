8a277b571bedb53eb96e5c494edbfcc47171e1e5
Christian Tanzer <tanzer@swing.co.at>
2013-11-17 12:55:07 +0100

    Simplify, use `file` to determine `input_encoding`

--- recode.py
+++ recode.py
@@ -1,5 +1,5 @@
-# -*- coding: iso-8859-15 -*-
-# Copyright (C) 2012 Mag. Christian Tanzer All rights reserved
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012-2013 Mag. Christian Tanzer All rights reserved
 # Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
 # #*** <License> ************************************************************
 # This module is free software; you can redistribute it and/or
@@ -25,7 +25,9 @@
 #
 # Revision Dates
 #    22-May-2012 (CT) Creation
-#    ««revision-date»»···
+#    17-Nov-2013 (CT) Simplify, use `file` to determine `input_encoding`
+#    18-Nov-2013 (CT) Add `coding_map` and option `-force`
+#    Â«Â«revision-dateÂ»Â»Â·Â·Â·
 #--
 
 from   __future__  import absolute_import, division, print_function, unicode_literals
@@ -37,49 +39,76 @@ from   _TFL.Regexp import Regexp, re
 
 import _TFL.CAO
 
-_cc_pat = Regexp \
+from   plumbum     import local as pbl
+
+coding_map = \
+    { "binary"       : "iso-8859-15"
+    , "iso-8859-1"   : "iso-8859-15"
+    , "unknown-8bit" : "iso-8859-15"
+    , "us-ascii"     : "iso-8859-15"
+    }
+
+coding_pat = Regexp \
     ( r"(?P<head> -\*- \s+ coding: \s+) (?P<encoding> \S+) (?P<tail> \s+ -\*-)"
     , re.VERBOSE
     )
 
-def recode (source, input_encoding, output_encoding) :
-    """Change encoding of `source` from `input_encoding` to `output_encoding`"""
-    cc_pat = _cc_pat
-    cc_rep = r"\g<head>%s\g<tail>" % output_encoding
-    if cc_pat.search (source) :
-        input_encoding = cc_pat.encoding
+mime_cmd   = pbl ["file"] ["--brief", "--mime"]
+mime_pat   = Regexp \
+    ( "(?P<type> [^/]+)/(?P<sub_type> [^;]+);"
+      "\s+"
+      "charset=(?P<encoding> [-a-zA-Z0-9]+)"
+    , re.VERBOSE
+    )
+
+def file_iter (names, output_encoding, force = False) :
+    for name in names :
+        t, s, input_encoding = file_mime_type (name)
+        if t == "text" or force:
+            input_encoding = coding_map.get (input_encoding, input_encoding)
+            if input_encoding != output_encoding :
+                yield name, input_encoding
+# end def file_iter
+
+def file_mime_type (name) :
+    result = mime_cmd (name)
+    if mime_pat.match (result) :
+        return mime_pat.type, mime_pat.sub_type, mime_pat.encoding
+    else :
+        return None, None, None
+# end def file_mime_type
+
+def recode_file \
+        (name, input_encoding, output_encoding, backup = None, verbose = False):
+    with open (name, "rb") as f :
+        source = f.read ()
+    coding_rep = r"\g<head>%s\g<tail>" % output_encoding
+    if coding_pat.search (source) :
+        input_encoding = coding_pat.encoding
     text = source.decode (input_encoding)
-    if cc_pat.last_match :
-        text = cc_pat.sub (cc_rep, text, 1)
-    return text.encode (output_encoding)
-# end def recode
+    if coding_pat.last_match :
+        text = coding_pat.sub (coding_rep, text, 1)
+    target = text.encode (output_encoding)
+    if backup :
+        sos.rename (name, name + backup)
+    with open (name, "w+b") as f :
+        f.write (target)
+    if verbose :
+        print \
+            ( "Converted %s from %s to %s"
+            % (name, input_encoding, output_encoding)
+            )
+# end def recode_file
 
 def _main (cmd) :
     backup          = cmd.backup
-    input_encoding  = cmd.input_encoding
+    force           = cmd.force
     output_encoding = cmd.output_encoding
+    verbose         = cmd.verbose
     if backup and not backup.startswith (".") :
         backup = "." + backup
-    for name in cmd.argv :
-        with open (name, "rb") as f :
-            source = f.read ()
-        target = recode (source, input_encoding, output_encoding)
-        if cmd.inplace :
-            if backup :
-                sos.rename (name, name + backup)
-            with open (name, "w+b") as f :
-                f.write (target)
-        elif cmd.target_dir :
-            if sos.path.isabs (name) :
-                _, name   = sos.path.split (name)
-            target_name   = sos.path.join  (cmd.target_dir, name)
-            target_dir, _ = sos.path.split (target_name)
-            if not sos.path.isdir (target_dir) :
-                sos.makedirs (target_dir)
-            with open (target_name, "wb") as f :
-                f.write (target)
-        else :
-            print (target)
+    for name, input_encoding in file_iter (cmd.argv, output_encoding, force) :
+        recode_file (name, input_encoding, output_encoding, backup, verbose)
 # end def _main
 
 _Command = TFL.CAO.Cmd \
@@ -89,17 +118,15 @@ _Command = TFL.CAO.Cmd \
         ,
         )
     , opts          =
-        ( "-backup:S=.bak?Save a backup of `file` with the extension given"
-        , "-inplace:B?Change `file` in place"
-        , TFL.CAO.Opt.Input_Encoding
-            ( description   = "Default encoding for source files"
-            , default       = "iso-8859-1"
-            )
+        ( "-backup:S?Save a backup of `file` with the extension given"
+        , "-force:B?"
+            "Force conversion even if `file --mime` indicates the file "
+            "isn't `text`"
         , TFL.CAO.Opt.Output_Encoding
             ( description   = "Output encoding"
             , default       = "utf-8"
             )
-        , "-target_dir:P?Target directory to write re-encoded files into"
+        , "-verbose:B?Print information about files recoded"
         )
     , min_args      = 1
     , description   = "Change file encoding"
