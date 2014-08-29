6b79dfb5a4de92783f1f7fedc1b15a566638f144
Christian Tanzer <tanzer@swing.co.at>
2014-08-29 17:16:10 +0200

    Filter warnings from `PIL`
    To avoid crap like this::
        /usr/lib/python2.7/site-packages/PIL/Image.py:44:
            DeprecationWarning: classic int division
          MAX_IMAGE_PIXELS = int(1024 * 1024 * 1024 / 4 / 3)

--- _ReST/__init__.py
+++ _ReST/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2010 Mag. Christian Tanzer. All rights reserved
+# Copyright (C) 2010-2014 Mag. Christian Tanzer. All rights reserved
 # Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
 # ****************************************************************************
 # This file is part of the package _ReST.
@@ -27,6 +27,7 @@
 #
 # Revision Dates
 #    15-Feb-2010 (CT) Creation
+#    29-Aug-2014 (CT) Filter warnings from `PIL`
 #    ««revision-date»»···
 #--
 
@@ -35,5 +36,14 @@ from _TFL.Package_Namespace import Package_Namespace
 ReST = Package_Namespace ()
 
 del Package_Namespace
+
+### Filter PIL warnings to avoid crap like this::
+# /usr/lib/python2.7/site-packages/PIL/Image.py:44:
+#     DeprecationWarning: classic int division
+#   MAX_IMAGE_PIXELS = int(1024 * 1024 * 1024 / 4 / 3)
+
+import warnings
+warnings.filterwarnings ("ignore", module = "^PIL.*")
+del warnings
 
 ### __END__ ReST.__init__
