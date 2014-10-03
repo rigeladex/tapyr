36bec93684d150b1cd471fbe5c32cce01af5631d
Christian Tanzer <tanzer@swing.co.at>
2014-10-03 19:02:39 +0200

    Modernize setup.py of TFL
    * Replace `svn` by `git`
    
    * Filter out `__pycache__` directories

--- _TFL/setup.py
+++ _TFL/setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (C) 2007 Mag. Christian Tanzer. All rights reserved
+# Copyright (C) 2007-2014 Mag. Christian Tanzer. All rights reserved
 # Glasauergasse 32, A--1130 Wien, Austria.
 # Web: http://www.c-tanzer.at/en/ Email: tanzer@swing.co.at
 # All rights reserved
@@ -20,53 +20,79 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 # ****************************************************************************
+#
+#++
+# Name
+#    _TFL.setup
+#
+# Purpose
+#    Setup file for package namespace TFL
+#
+# Revision Dates
+#    25-Oct-2007 (RS) Creation
+#     7-Nov-2007 (CT) Add info, use `if` instead of `assert`
+#    26-May-2011 (CT) Change file encoding to `iso-8859-15`
+#    18-Nov-2013 (CT) Change file encoding to `utf-8`
+#     3-Oct-2014 (CT) Replace `svn` by `git`
+#     3-Oct-2014 (CT) Filter out `__pycache__` directories
+#    ««revision-date»»···
+#--
+
+from   __future__               import print_function
 
 import os
 import sys
-from   distutils.core import setup
+from   distutils.core           import setup
 
 src_dir = os.path.dirname (__file__)
 if src_dir :
     os.chdir (src_dir)
 
-name    = 'TFL'
 license = 'GNU Library or Lesser General Public License (LGPL)'
-version = os.popen ('svnversion').read ().strip ()
-if version.isdigit () :
-    pkgs    = []
-    # path-walking probably only works in Unix due to hardcoded '/'
-    for root, dirs, files in os.walk ('../_' + name) :
-        # ugly: need to modify dirs in place!
-        dd = set (d for d in dirs if d.startswith ('_'))
-        for d in dirs :
-            if d not in dd :
-                dirs.remove (d)
-        pkgs.append (root.replace ('/', '.') [3:])
-    setup \
-        ( name             = name
-        , version          = version
-        , description      =
-            "Library with lots of useful stuff (says Ralf Schlatterbeck)"
-        # long_description =
-        , license          = license
-        , author           = "Christian Tanzer and others"
-        , author_email     = "tanzer@swing.co.at"
-        , url              = "http://www.c-tanzer.at/en"
-        , packages         = pkgs
-        , package_dir      = {'_' + name : ''}
-        , platforms        = 'Any'
-        , classifiers      = \
-            [ 'Development Status :: 6 - Mature'
-            , 'License :: OSI Approved :: ' + license
-            , 'Operating System :: OS Independent'
-            , 'Programming Language :: Python'
-            , 'Intended Audience :: Developers'
-            , 'Topic :: Software Development :: Libraries'
-            , 'Topic :: Software Development :: Libraries :: '
-                'Application Frameworks'
-            , 'Topic :: Software Development :: Libraries :: Python Modules'
-            ]
-        )
-else :
-    print "SVN version `%s` is not amenable to using setup" % (version, )
-    print "   Please check-in your changes, if any, and update your sandbox"
+
+name    = "TFL"
+p_name  = "_TFL"
+v_name  = "TFL-version"
+
+try :
+    with open (v_name, "r") as f :
+        version = f.read ().strip ()
+except IOError :
+    print ("No", v_name, "file found")
+    raise SystemExit (1)
+
+packages = []
+for root, dirs, files in os.walk (os.path.join ('..', p_name)) :
+    # ugly: need to modify dirs in place!
+    dd = set (d for d in dirs if d.startswith ('_') and d != "__pycache__")
+    for d in dirs :
+        if d not in dd :
+            dirs.remove (d)
+    packages.append (root.replace ('/', '.').strip ("."))
+
+setup \
+    ( name                 = name
+    , version              = version
+    , description          =
+        "Library with lots of useful stuff (says Ralf Schlatterbeck)"
+    # long_description     =
+    , license              = license
+    , author               = "Christian Tanzer and others"
+    , author_email         = "tanzer@swing.co.at"
+    , url                  = "https://github.com/Tapyr/tapyr"
+    , packages             = packages
+    , package_dir          = {p_name : ""}
+    , platforms            = 'Any'
+    , classifiers          = \
+        [ 'Development Status :: 6 - Mature'
+        , 'License :: OSI Approved :: ' + license
+        , 'Operating System :: OS Independent'
+        , 'Programming Language :: Python :: 2'
+        , 'Programming Language :: Python :: 3'
+        , 'Intended Audience :: Developers'
+        , 'Topic :: Software Development :: Libraries'
+        , 'Topic :: Software Development :: Libraries :: '
+            'Application Frameworks'
+        , 'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+    )
