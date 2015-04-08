rigeladex/tapyr

commit 8e25eb429c19ca46d55a5a80647c2c449738b2f8

Change license to BSD 3-Clause License
The package namespaces CAL, DJO, PMA, TFL, and TGL are changed from the
GNU Library/Lesser General Public License to the BSD 3-clause license.

The package namespaces ATAX, GTW, JNJ, LNX, MOM, and ReST are changed
from the GNU Affero General Public License to the BSD 3-clause license.

Add rewrite_license_header.py to change the license notices in the
header comments of source files. Due to hysterical raisins,
`rewrite_license_header` needs a lot of special casing. In general, I
used it like this::

    python rewrite_license_header.py $(git grep -l 'MIT License or')

    python rewrite_license_header.py $(git grep -l 'GNU General ')

_TGL/_TKT/_GTK/_wrapper_generator.py
@@ -3,63 +3,8 @@
 # Langstrasse 4, A--2244 Spannberg, Austria. office@spannberg.com
 # ****************************************************************************
 #
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Library General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Library General Public License for more details.
-#
-# You should have received a copy of the GNU Library General Public
-# License along with this library; if not, write to the Free
-# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-# ****************************************************************************
-#
-#++
-# Name
-#    TGL.TKT.GTK._wrapper_generator
-#
-# Purpose
-#    Generate the the raw wrapper file for a gtk widget
-#
-# Revision Dates
-#    22-Mar-2005 (MG) Creation
-#    27-Mar-2005 (MG) `find_properties` fixed
-#    27-Mar-2005 (MG) Correct auto base name
-#    27-Mar-2005 (MG) Support for `SG_Object_Property` added
-#    ««revision-date»»···
-#--
-
-import gtk
-import gobject
-import sys
-import os
-import time
-
-from   _TFL.Command_Line import Command_Line
-from   _TFL.Regexp       import Regexp
-
-template = '''# -*- coding: iso-8859-15 -*-
-# Copyright (C) 2005 Martin Glück. All rights reserved
-# Langstrasse 4, A--2244 Spannberg, Austria. office@spannberg.com
-# ****************************************************************************
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Library General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Library General Public License for more details.
-#
-# You should have received a copy of the GNU Library General Public
-# License along with this library; if not, write to the Free
-# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# This module is licensed under the terms of the BSD 3-Clause License
+# <http://www.c-tanzer.at/license/bsd_3c.html>.
 # ****************************************************************************
 #
 #++

_TGL/_TKT/_HILD/_wrapper_generator.py
@@ -3,64 +3,8 @@
 # Langstrasse 4, A--2244 Spannberg, Austria. office@spannberg.com
 # ****************************************************************************
 #
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Library General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Library General Public License for more details.
-#
-# You should have received a copy of the GNU Library General Public
-# License along with this library; if not, write to the Free
-# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-# ****************************************************************************
-#
-#++
-# Name
-#    TGL.TKT.HILD._wrapper_generator
-#
-# Purpose
-#    Generate the the raw wrapper file for a HILD widget
-#
-# Revision Dates
-#    22-Mar-2005 (MG) Creation
-#    27-Mar-2005 (MG) `find_properties` fixed
-#    27-Mar-2005 (MG) Correct auto base name
-#    27-Mar-2005 (MG) Support for `SG_Object_Property` added
-#    ««revision-date»»···
-#--
-
-import gtk
-import gobject
-import sys
-import os
-import time
-import hildon
-
-from   _TFL.Command_Line import Command_Line
-from   _TFL.Regexp       import Regexp
-
-template = '''# -*- coding: iso-8859-15 -*-
-# Copyright (C) 2005 Martin Glück. All rights reserved
-# Langstrasse 4, A--2244 Spannberg, Austria. office@spannberg.com
-# ****************************************************************************
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Library General Public
-# License as published by the Free Software Foundation; either
-# version 2 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Library General Public License for more details.
-#
-# You should have received a copy of the GNU Library General Public
-# License along with this library; if not, write to the Free
-# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# This module is licensed under the terms of the BSD 3-Clause License
+# <http://www.c-tanzer.at/license/bsd_3c.html>.
 # ****************************************************************************
 #
 #++

commit dd03dcea0824277b1bf7b4594e16240d108938da

Fix syntax errors introduced by 8e25eb429c19

_TGL/_TKT/_GTK/_wrapper_generator.py
@@ -1,8 +1,40 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2005 Martin Glück. All rights reserved
+# Copyright (C) 2005-2015 Martin Glück. All rights reserved
 # Langstrasse 4, A--2244 Spannberg, Austria. office@spannberg.com
 # ****************************************************************************
+# This module is licensed under the terms of the BSD 3-Clause License
+# <http://www.c-tanzer.at/license/bsd_3c.html>.
+# ****************************************************************************
 #
+#++
+# Name
+#    TGL.TKT.GTK._wrapper_generator
+#
+# Purpose
+#    Generate the the raw wrapper file for a gtk widget
+#
+# Revision Dates
+#    22-Mar-2005 (MG) Creation
+#    27-Mar-2005 (MG) `find_properties` fixed
+#    27-Mar-2005 (MG) Correct auto base name
+#    27-Mar-2005 (MG) Support for `SG_Object_Property` added
+#     8-Apr-2015 (CT) Fix syntax errors introduced by commit 8e25eb429c19
+#    ««revision-date»»···
+#--
+
+import gtk
+import gobject
+import sys
+import os
+import time
+
+from   _TFL.Command_Line import Command_Line
+from   _TFL.Regexp       import Regexp
+
+template = '''# -*- coding: iso-8859-15 -*-
+# Copyright (C) 2005-2015 Martin Glück. All rights reserved
+# Langstrasse 4, A--2244 Spannberg, Austria. office@spannberg.com
+# ****************************************************************************
 # This module is licensed under the terms of the BSD 3-Clause License
 # <http://www.c-tanzer.at/license/bsd_3c.html>.
 # ****************************************************************************

_TGL/_TKT/_HILD/_wrapper_generator.py
@@ -1,7 +1,42 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2005 Martin Glück. All rights reserved
+# Copyright (C) 2005-2015 Martin Glück. All rights reserved
 # Langstrasse 4, A--2244 Spannberg, Austria. office@spannberg.com
 # ****************************************************************************
+# This module is licensed under the terms of the BSD 3-Clause License
+# <http://www.c-tanzer.at/license/bsd_3c.html>.
+# ****************************************************************************
+#
+#++
+# Name
+#    TGL.TKT.HILD._wrapper_generator
+#
+# Purpose
+#    Generate the the raw wrapper file for a HILD widget
+#
+# Revision Dates
+#    22-Mar-2005 (MG) Creation
+#    27-Mar-2005 (MG) `find_properties` fixed
+#    27-Mar-2005 (MG) Correct auto base name
+#    27-Mar-2005 (MG) Support for `SG_Object_Property` added
+#     8-Apr-2015 (CT) Fix syntax errors introduced by commit 8e25eb429c19
+#    ««revision-date»»···
+#--
+
+import gtk
+import gobject
+import sys
+import os
+import time
+import hildon
+
+from   _TFL.Command_Line import Command_Line
+from   _TFL.Regexp       import Regexp
+
+template = '''# -*- coding: iso-8859-15 -*-
+# Copyright (C) 2005-2015 Martin Glück. All rights reserved
+# Langstrasse 4, A--2244 Spannberg, Austria. office@spannberg.com
+# ****************************************************************************
+# This module is part of the package MOM.Attr.
 #
 # This module is licensed under the terms of the BSD 3-Clause License
 # <http://www.c-tanzer.at/license/bsd_3c.html>.