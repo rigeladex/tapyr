f1ea6355969d3d3b46469662e51d8e7bd73f4473
Christian Tanzer <tanzer@swing.co.at>
2017-02-28 19:16:08 +0100

    Add `range` method to TFL.DRA.Binner

--- _TFL/_DRA/Binner.py
+++ _TFL/_DRA/Binner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2006-2016 Mag. Christian Tanzer. All rights reserved
+# Copyright (C) 2006-2017 Mag. Christian Tanzer. All rights reserved
 # Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
 # ****************************************************************************
 # This module is licensed under the terms of the BSD 3-Clause License
@@ -19,6 +19,7 @@
 #    24-Nov-2006 (CT) Optional argument `center` added to `value`
 #    26-Nov-2006 (CT) `index_f` factored
 #     9-Oct-2016 (CT) Move to Package_Namespace `TFL`
+#    28-Feb-2017 (CT) Add `range` method
 #    ««revision-date»»···
 #--
 
@@ -30,15 +31,20 @@ import _TFL._DRA
 class Binner (TFL.Meta.Object) :
     """Distribute measurement values into bins.
 
-       The range of possible values is split into bins that are numbered from
-       1 to `n` (0 is reserved for invalid values). Each measured value is
-       mapped to the bin containing it.
+    The range of possible values is split into bins that are numbered from
+    1 to `n` (0 is reserved for invalid values). Each measured value is
+    mapped to the bin containing it.
 
-       >>> B = Binner (0, 2)
-       >>> [B.index (d) for d in (0, 1, 2, 10, 180, 359)]
-       [1, 1, 2, 6, 91, 180]
-       >>> [B.value (i) for i in [1, 1, 2, 6, 91, 180]]
-       [1.0, 1.0, 3.0, 11.0, 181.0, 359.0]
+    >>> B = Binner (0, 2)
+    >>> [B.index (d) for d in (0, 1, 2, 10, 180, 359)]
+    [1, 1, 2, 6, 91, 180]
+
+    >>> [B.value (i) for i in [1, 1, 2, 6, 91, 180]]
+    [1.0, 1.0, 3.0, 11.0, 181.0, 359.0]
+
+    >>> [B.range (i) for i in [1, 2, 6, 91, 180]]
+    [(0.0, 2.0), (2.0, 4.0), (10.0, 12.0), (180.0, 182.0), (358.0, 360.0)]
+
     """
 
     def __init__ (self, offset, width) :
@@ -60,6 +66,13 @@ class Binner (TFL.Meta.Object) :
         """Returns bin index for value `r` as float value."""
         return ((r - self.offset) / self.width) + 1
     # end def index
+
+    def range (self, i) :
+        """Returns the range of values in bin `i`."""
+        lo = self.value (i, center = False)
+        hi = lo + self.width
+        return (lo, hi)
+    # end def range
 
     def value (self, i, center = True) :
         """Returns value corresponding to bin `i`."""
