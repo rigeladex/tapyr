17125a0ae9eb37fdb06eb51fbd605ffaecf716ff
Christian Tanzer <tanzer@swing.co.at>
2017-05-08 19:07:42 +0200

    Add modules App_Test and run_app_tests to TFL

---
+++ run_app_tests.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017 Mag. Christian Tanzer All rights reserved
+# Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
+# #*** <License> ************************************************************
+# This module is licensed under the terms of the BSD 3-Clause License
+# <http://www.c-tanzer.at/license/bsd_3c.html>.
+# #*** </License> ***********************************************************#
+#
+#++
+# Name
+#    run_app_tests
+#
+# Purpose
+#    Run application tests in all modules specified by command line
+#
+# Revision Dates
+#     8-May-2017 (CT) Creation
+#    ««revision-date»»···
+#--
+
+from   __future__         import absolute_import
+from   __future__         import division
+from   __future__         import print_function
+from   __future__         import unicode_literals
+
+from   _TFL.run_app_tests import Command
+
+if __name__ == "__main__" :
+    Command ()
+### __END__ run_app_tests
