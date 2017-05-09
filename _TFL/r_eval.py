92616edc6a8f34da95a3b179e3d71ffd5beefc43
Christian Tanzer <tanzer@swing.co.at>
2017-05-09 10:47:27 +0200

    Make Python-3 compatible for both `str` & `bytes`

--- _TFL/r_eval.py
+++ _TFL/r_eval.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2012-2013 Mag. Christian Tanzer All rights reserved
+# Copyright (C) 2012-2017 Mag. Christian Tanzer All rights reserved
 # Glasauergasse 32, A--1130 Wien, Austria. tanzer@swing.co.at
 # #*** <License> ************************************************************#
 # This module is part of the package TFL.
-# 
+#
 # This module is licensed under the terms of the BSD 3-Clause License
 # <http://www.c-tanzer.at/license/bsd_3c.html>.
 # #*** </License> ***********************************************************#
@@ -20,31 +20,37 @@
 #     8-Jun-2012 (CT) Add `\s*` to `_eval_restrict_pat`
 #     6-Jun-2013 (CT) Change guard to `isinstance (source, pyk.string_types)`
 #     4-Dec-2013 (CT) Remove `coding` cookie from unicode values
+#     9-May-2017 (CT) Make Python-3 compatible for both `str` and `bytes`
 #    ««revision-date»»···
 #--
 
-from   __future__  import absolute_import, division, print_function
+from   __future__         import absolute_import, division, print_function
 
-from   _TFL        import TFL
-from   _TFL.pyk    import pyk
+from   _TFL               import TFL
+from   _TFL.portable_repr import portable_repr
+from   _TFL.pyk           import pyk
 
 import re
 
 _coding_pat        = re.compile \
-    ( r"^# *(-\*-)?\s*coding[:=]\s*[-a-zA-Z0-9]+ *(-\*-)? *" + "\n"
+    ( br"^# *(-\*-)?\s*coding[:=]\s*(?P<enc>[-a-zA-Z0-9]+) *(-\*-)? *" + b"\n"
     , re.MULTILINE
     )
 
 _eval_restrict_pat = re.compile \
-    ( r"(?: (?: ^|\W)(?: lambda)(?: \W|$))|\.\s*__|(?: ^|\W)inspect\."
+    ( pyk.decoded
+        (r"(?: (?: ^|\W)(?: lambda)(?: \W|$))|\.\s*__|(?: ^|\W)inspect\.")
     , re.VERBOSE
     )
 
 def r_eval (source, ** kw) :
     """Evaluate `source`, in a scope with values of `kw`, but nothing else.
 
-
     >>> r_eval ("2 * 2")
+    4
+    >>> r_eval (u"2 * 2")
+    4
+    >>> r_eval (b"2 * 2")
     4
     >>> r_eval ("dir ()")
     Traceback (most recent call last):
@@ -69,12 +75,16 @@ def r_eval (source, ** kw) :
     ValueError: Cannot safely evaluate '((). __class__)'
     """
     if isinstance (source, pyk.text_type) :
-        source = _coding_pat.sub ("", source, 1)
-    if isinstance (source, pyk.string_types) :
-        if _eval_restrict_pat.search (source) :
-            raise ValueError ("Cannot safely evaluate %r" % source)
-        scope = dict (kw, __builtins__ = {})
-        return eval (source, scope)
+        src     = source
+    else :
+        c_match = _coding_pat.search (source)
+        enc     = (c_match.group ("enc"), ) if c_match else ()
+        src     = _coding_pat.sub ("", source, 1)
+        src     = pyk.decoded (src, * enc)
+    if _eval_restrict_pat.search (src) :
+        raise ValueError ("Cannot safely evaluate %r" % (source [:60], ))
+    scope = dict (kw, __builtins__ = {})
+    return eval (src, scope)
 # end def r_eval
 
 if __name__ != "__main__" :
